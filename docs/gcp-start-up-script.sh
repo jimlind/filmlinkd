9281df1f9330271c8701e69aad59958135358b52
Jim Lind <spoon.vw@gmail.com>
2021-04-07 23:35:11 -0400

    Add Node Install to Startup Script
    We need it.

--- docs/gcp-start-up-script.sh
+++ docs/gcp-start-up-script.sh
@@ -6,7 +6,7 @@
 logging_agent_install_file="/tmp/google-fluentd-install.sh" 
 curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh -o $logging_agent_install_file
 bash $logging_agent_install_file
-#rm /tmp/google-fluentd-install.sh
+rm /tmp/google-fluentd-install.sh
 apt-get update
 apt-get install -y google-fluentd
 apt-get install -y google-fluentd-catch-all-config
@@ -19,12 +19,14 @@ service google-fluentd restart
 monitoring_agent_install_file="/tmp/add-monitoring-agent-repo.sh"
 curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh -o $monitoring_agent_install_file
 bash $monitoring_agent_install_file
-#rm /tmp/add-monitoring-agent-repo.sh
+rm /tmp/add-monitoring-agent-repo.sh
 apt-get update
 apt-get install -y stackdriver-agent
 service stackdriver-agent start
 service stackdriver-agent restart
 
-# Example Google Install Scripts
-#curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh && sudo bash add-logging-agent-repo.sh && sudo apt-get update && sudo apt-get install google-fluentd && sudo apt-get install -y google-fluentd-catch-all-config && sudo service google-fluentd start"
-#curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh && sudo bash add-monitoring-agent-repo.sh && sudo apt-get update && sudo apt-get install stackdriver-agent && sudo service stackdriver-agent start"
+###
+### Install Node 14.x
+###
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
