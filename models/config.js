08d29360d3663c161268f5384637a58caacb1743
Jim Lind <spoon.vw@gmail.com>
2021-04-09 22:01:00 -0400

    Add the folder of models. I know there should be more. Probably will be later.

---
+++ models/config.js
@@ -0,0 +1,16 @@
+class Config {
+    discordBotToken = "";
+    firestoreCollectionId = "";
+    gcpKeyFile = "";
+    googleCloudProjectId = "";
+    isDev = false;
+    isVIP = false;
+
+    packageName = "";
+    packageVersion = "";
+
+    rssDelay = 1000;
+    pageSize = 60;
+}
+
+module.exports = Config;
---
+++ models/user-text-message.js
@@ -0,0 +1,9 @@
+class UserTextMessage {
+    manageServer = false;
+    command = "";
+    arguments = "";
+    channelId = 0;
+    guildId = 0;
+}
+
+module.exports = UserTextMessage;
