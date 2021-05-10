9c92fc86002fbca0a748dba03f0984819365a4fa
Jim Lind <spoon.vw@gmail.com>
2021-05-10 19:45:07 -0400

    Undo Logger JS Delete
    Need to go back.

---
+++ dist/services/logger.js
@@ -0,0 +1,28 @@
+'use strict';
+class Logger {
+    constructor(config, winston, googleCloudWinstonTransport, consoleTransport) {
+        // Create a Winston logger that logs debug and above
+        this.winstonLogger = winston.createLogger({
+            level: 'debug',
+            format: winston.format.combine(winston.format.timestamp(), winston.format.prettyPrint()),
+            transports: [googleCloudWinstonTransport],
+        });
+        // If the environment is dev, then also log to console
+        if (config.getIsDev()) {
+            this.winstonLogger.add(consoleTransport);
+        }
+    }
+    debug(...args) {
+        this.winstonLogger.debug(...args);
+    }
+    info(...args) {
+        this.winstonLogger.info(...args);
+    }
+    warn(...args) {
+        this.winstonLogger.warn(...args);
+    }
+    error(...args) {
+        this.winstonLogger.error(...args);
+    }
+}
+module.exports = Logger;
