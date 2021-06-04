6a3f37fe90d9ed5aa69fcf40812d72764adf3814
Jim Lind <spoon.vw@gmail.com>
2021-06-03 20:36:50 -0400

    Fix and Format User Delete Script
    Helps clear up some bad user data.

--- tools/delete-bad-users.js
+++ tools/delete-bad-users.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
-const ConfigFactory = require("../factories/config-factory");
-const DependencyInjectionContainer = require("../dependency-injection-container");
-const dotenv = require("dotenv");
-const fs = require("fs");
-const { LetterboxdDiary } = require("letterboxd");
+const ConfigFactory = require('../factories/config-factory');
+const DependencyInjectionContainer = require('../dependency-injection-container');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
 // Load .env into process.env, create config, create container
 dotenv.config();
-const configModel = new ConfigFactory("prod", process.env, {}, fs.existsSync).build();
+const configModel = new ConfigFactory('prod', process.env, {}, fs.existsSync).build();
 const container = new DependencyInjectionContainer(configModel);
 
-const collection = container.resolve("firestoreConnection").getCollection();
+const collection = container.resolve('firestoreConnection').getCollection();
 processData(collection);
 
 async function processData(collection) {
     const querySnapshot = await collection.get();
     var documentList = querySnapshot.docs;
-    const fileName = "Bad User Deletes " + new Date().toString() + ".txt";
+    const fileName = 'Bad User Deletes ' + new Date().toString() + '.txt';
 
     for (var i = 0; i < 10; i++) {
         console.log(`${documentList.length} users to check...`);
         for (const key in documentList) {
             const data = documentList[key].data();
             try {
-                await container.resolve("letterboxdDiary").get(data.userName, 1);
+                await container.resolve('letterboxdDiaryRss').get(data.userName, 1);
                 console.log(`+++ Success on ${data.userName}`);
                 documentList[key] = false;
             } catch (error) {
@@ -38,8 +37,8 @@ async function processData(collection) {
         const document = documentList[key];
         const data = document.data();
 
-        const content = data.userName + "||" + JSON.stringify(data.channelList) + "\n";
-        fs.writeFileSync(fileName, content, { flag: "a+" }, (err) => {});
+        const content = data.userName + '||' + JSON.stringify(data.channelList) + '\n';
+        fs.writeFileSync(fileName, content, { flag: 'a+' }, (err) => {});
         await document.ref.delete();
     }
 }
