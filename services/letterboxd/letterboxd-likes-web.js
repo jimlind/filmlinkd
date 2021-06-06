87ea8e2fff6ea46dba6544de2d408c1eba6abc9f
Jim Lind <spoon.vw@gmail.com>
2021-06-05 23:34:30 -0400

    Update Likes Logic
    Parse the data from the activity ajax requests instead of the film liked page. The film liked page didn't have things in an order that really made sense.

--- services/letterboxd/letterboxd-likes-web.js
+++ services/letterboxd/letterboxd-likes-web.js
@@ -1,33 +1,83 @@
 'use strict';
 
 class LetterboxdLikesWeb {
+    root = 'https://letterboxd.com';
+
     constructor(axios, htmlParser2) {
         this.axios = axios;
         this.htmlParser2 = htmlParser2;
         this.domUtils = htmlParser2.DomUtils;
     }
 
-    get(userName, count) {
+    get(userName, linkList, initialLikedList = [], after = 0) {
         return new Promise((resolve, reject) => {
-            this.axios
-                .get(`https://letterboxd.com/${userName}/likes/films/`)
-                .then((response) => {
-                    const dom = this.htmlParser2.parseDocument(response.data);
-                    const posterList = this.domUtils.getElements(
-                        // Use `tag_contains` here so tag and class are both checked against
-                        { tag_contains: 'div', class: 'poster film-poster really-lazy-load' },
-                        dom,
-                        true,
-                        count,
-                    );
+            this.fetch(userName, after)
+                .then(({ likedList, activityId, endOfActivity }) => {
+                    const completeLikedList = { ...initialLikedList, ...likedList };
+                    const keys = Object.keys(completeLikedList);
 
-                    const filmSlugList = [];
-                    posterList.forEach((poster) => {
-                        const filmSlug = this.domUtils.getAttributeValue(poster, 'data-film-slug');
-                        filmSlugList.push(filmSlug);
+                    let unfoundLinks = linkList.filter((element) => {
+                        return !keys.includes(element);
                     });
 
-                    resolve(filmSlugList);
+                    if (unfoundLinks.length == 0 || endOfActivity) {
+                        const likedFilmData = Object.entries(completeLikedList);
+                        const likedFilmList = likedFilmData.reduce((accumulator, current) => {
+                            return current[1] ? [...accumulator, current[0]] : accumulator;
+                        }, []);
+                        resolve(likedFilmList);
+                    } else {
+                        resolve(this.get(userName, linkList, completeLikedList, activityId));
+                    }
+                })
+                .catch(reject);
+        });
+    }
+
+    fetch(userName, after) {
+        const url =
+            `${this.root}/ajax/activity-pagination/${userName}/` + (after ? `?after=${after}` : '');
+
+        return new Promise((resolve, reject) => {
+            this.axios
+                .get(url)
+                .then((response) => {
+                    const likedList = {};
+                    const dom = this.htmlParser2.parseDocument(response.data);
+
+                    // Parse reviewed films from activity
+                    this.domUtils
+                        .getElements({ class: 'film-detail-content' }, dom)
+                        .forEach((detail) => {
+                            const h2 = this.domUtils.getElementsByTagName('h2', detail)[0];
+                            const a = this.domUtils.getElementsByTagName('a', h2)[0];
+                            const href = this.root + this.domUtils.getAttributeValue(a, 'href');
+                            const liked = this.domUtils.getElements(
+                                { class: 'has-icon icon-16 icon-liked' },
+                                detail,
+                            );
+
+                            likedList[href] = Boolean(liked.length);
+                        });
+
+                    // Parse watched films from activity
+                    this.domUtils
+                        .getElements({ tag_contains: 'a', class: 'target' }, dom)
+                        .forEach((anchor) => {
+                            const href =
+                                this.root + this.domUtils.getAttributeValue(anchor, 'href');
+                            if (href.startsWith(`${this.root}/${userName}/`)) {
+                                const options = { class: 'context' };
+                                const context = this.domUtils.getElements(options, anchor);
+                                likedList[href] = this.domUtils.getText(context).includes('liked');
+                            }
+                        });
+
+                    // Parse activity id from sections
+                    const section = this.domUtils.getElementsByTagName('section', dom).pop();
+                    const activityId = this.domUtils.getAttributeValue(section, 'data-activity-id');
+
+                    resolve({ likedList, activityId, endOfActivity: !activityId });
                 })
                 .catch(reject);
         });
