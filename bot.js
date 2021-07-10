5b8b6f51ee69e1f64dfc6847f48735f8029ee597
Jim Lind <spoon.vw@gmail.com>
2021-07-10 15:34:12 -0400

    Feeling good enough about the state of this to commit.
    Connecting the pipes up even though the writing stuff isn't completed.

--- bot.js
+++ bot.js
@@ -2,11 +2,12 @@
 
 const ConfigFactory = require('./factories/config-factory');
 const DependencyInjectionContainer = require('./dependency-injection-container');
+const DiaryEntry = require('./models/diary-entry');
 const death = require('death');
 const dotenv = require('dotenv');
 const fs = require('fs');
 
-// @ts-ignore
+// @ts-ignore TypeScript validation doesn't like json files as a module
 const packageJson = require('./package.json');
 
 // Load .env into process.env, create config, create container
@@ -17,63 +18,66 @@ const configModel = new ConfigFactory(
     packageJson,
     fs.existsSync,
 ).build();
+
+// Make sure the promises for PubSub and Discord Clients connect
 const container = new DependencyInjectionContainer(configModel);
+Promise.all([
+    container.resolve('discordConnection').getConnectedClient(),
+    container.resolve('pubSubConnection').getSubscription(),
+]).then(([discordClient]) => {
+    const serverCount = discordClient.guilds.cache.size;
+    container.resolve('logger').info(`Discord Client Logged In on ${serverCount} Servers`);
 
-container
-    .resolve('discordConnection')
-    .getConnectedClient()
-    .then((discordClient) => {
-        const serverCount = discordClient.guilds.cache.size;
-        container.resolve('logger').info(`Discord Client Logged In on ${serverCount} Servers`);
-
-        // Listen for discord messages posted and respond
-        container.resolve('discordMessageListener').onMessage((message) => {
-            container.resolve('actionTranslator').translate(message);
-        });
-
-        /*
-        // Listen for PubSub messages posted and respond
-        container.resolve('pubSubMessageListener').onMessage((message) => {
-            // TODO:
-            // This shouldn't actually be a "write entry" action.
-            // This should be a "here is a potential new entry" action.
-            container.resolve('diaryEntryWriter').write(message);
-        });
-        */
-
-        // This is the part that posts RSS updates at a regular interval
-        // Keeps track of if an active diary entry thread is running
-        let threadRunning = false;
-        let interval = null;
-
-        // Get a random index from the user list
-        container
-            .resolve('subscribedUserList')
-            .getRandomIndex()
-            .then((index) => {
-                const diaryRestInterval = 30 * 1000; // Give it 30 seconds to rest
-                interval = setInterval(() => {
-                    if (threadRunning) return;
-
-                    const config = container.resolve('config');
-                    container
-                        .resolve('diaryEntryProcessor')
-                        .processPageOfEntries(index, config.pageSize)
-                        .then((pageCount) => {
-                            threadRunning = false;
-                            index = pageCount === 0 ? 0 : index + pageCount;
-                        });
-
-                    threadRunning = true;
-                }, diaryRestInterval);
-            });
-
-        // Clean up when process is told to end
-        death((signal, error) => {
-            clearInterval(interval);
-            discordClient.destroy();
-            // This seems to fail.
-            container.resolve('pubSubConnection').getSubscription().close();
-            container.resolve('logger').info('Program Terminated', { signal, error });
-        });
+    // Listen for discord messages posted and respond
+    container.resolve('discordMessageListener').onMessage((message) => {
+        container.resolve('actionTranslator').translate(message);
     });
+
+    // Listen for PubSub messages posted and respond
+    container.resolve('pubSubMessageListener').onMessage((message) => {
+        const returnData = JSON.parse(message.data.toString());
+        const diaryEntry = Object.assign(new DiaryEntry(), returnData);
+        container
+            .resolve('diaryEntryWriter')
+            .validateAndWrite(diaryEntry)
+            .then(() => {
+                message.ack();
+            });
+    });
+
+    // This is the part that posts RSS updates at a regular interval
+    // Keeps track of if an active diary entry thread is running
+    let threadRunning = false;
+    let interval = null;
+
+    // Get a random index from the user list
+    container
+        .resolve('subscribedUserList')
+        .getRandomIndex()
+        .then((index) => {
+            const diaryRestInterval = 30 * 1000; // Give it 30 seconds to rest
+            interval = setInterval(() => {
+                if (threadRunning) return;
+
+                const config = container.resolve('config');
+                container
+                    .resolve('diaryEntryProcessor')
+                    .processPageOfEntries(index, config.pageSize)
+                    .then((pageCount) => {
+                        threadRunning = false;
+                        index = pageCount === 0 ? 0 : index + pageCount;
+                    });
+
+                threadRunning = true;
+            }, diaryRestInterval);
+        });
+
+    // Clean up when process is told to end
+    death((signal, error) => {
+        clearInterval(interval);
+        discordClient.destroy();
+        // TODO subscription close fails here. Not sure what's up.
+        container.resolve('pubSubConnection').getSubscription().close();
+        container.resolve('logger').info('Program Terminated', { signal, error });
+    });
+});
