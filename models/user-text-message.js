95da62091955c2d64ca54cdb483ba5fc7290dda0
Jim Lind <spoon.vw@gmail.com>
2021-07-18 22:11:09 -0400

    Document Some Data Types

--- models/user-text-message.js
+++ models/user-text-message.js
@@ -1,9 +1,12 @@
 class UserTextMessage {
     manageServer = false;
-    command = "";
-    arguments = "";
-    channelId = 0;
-    guildId = 0;
+    command = '';
+    /** @property {string[]} arguments - All strings passed in via command */
+    arguments = [];
+    /** @property {string} channelId - Message's channel Id stored as string to avoid silly rounding */
+    channelId = '';
+    /** @property {string} guildId - Message's guild Id stored as string to avoid silly rounding */
+    guildId = '';
 }
 
 module.exports = UserTextMessage;
