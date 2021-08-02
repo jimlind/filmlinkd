f6bad4a2a0ed4f608d82ef8aad9a8a34b115d5a7
Jim Lind <spoon.vw@gmail.com>
2021-08-01 22:39:07 -0400

    Create bot-vip.js

---
+++ bot-vip.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const ConfigFactory = require('./factories/config-factory');
+const DependencyInjectionContainer = require('./dependency-injection-container');
+const death = require('death');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// @ts-ignore TypeScript validation doesn't like json files as a module
+const packageJson = require('./package.json');
+
+// Load .env into process.env, create config, create container
+dotenv.config();
+const configModel = new ConfigFactory(
+    process.argv[2],
+    process.env,
+    packageJson,
+    fs.existsSync,
+).build();
+
+// Make sure the promises for PubSub and Discord Clients connect
+const container = new DependencyInjectionContainer(configModel);
+
+// This is the part that posts RSS updates at a regular interval
+// Keeps track of if an active diary entry thread is running
+let threadRunning = false;
+let interval = null;
+let index = 0;
+
+Promise.all([container.resolve('pubSubConnection').getSubscription()]).then(
+    ([pubSubSubscription]) => {
+        interval = setInterval(() => {
+            if (threadRunning) return; // Exit early
+            container
+                .resolve('diaryEntryProcessor')
+                .processPageOfVipEntries(index, 50)
+                .then((pageCount) => {
+                    threadRunning = false;
+                    index = pageCount === 0 ? 0 : index + pageCount;
+                });
+
+            threadRunning = true;
+        }, 1000); // Allow bot to rest for 1 second
+
+        // Clean up when process is told to end
+        death((signal, error) => {
+            clearInterval(interval);
+            pubSubSubscription.close();
+            container.resolve('logger').info('Program Terminated', { signal, error });
+        });
+    },
+);
