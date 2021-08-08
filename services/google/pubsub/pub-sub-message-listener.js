2fd82041fc0ffc62b0b81961e2021cd48992a69b
Jim Lind <spoon.vw@gmail.com>
2021-08-08 16:43:54 -0400

    Keep a local record of last 1000 messages
    And don't repeat sending the message if it is on the list. Since Pub/Sub doesn't have any support for native deduplication this is as lose as I can get.

--- services/google/pubsub/pub-sub-message-listener.js
+++ services/google/pubsub/pub-sub-message-listener.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const crypto = require('crypto');
+
 class PubSubMessageListener {
+    /** @type string[] */
+    hashList = [];
+
     constructor(pubSubConnection) {
         this.pubSubConnection = pubSubConnection;
     }
@@ -9,9 +14,23 @@ class PubSubMessageListener {
      * @param {function} callback
      */
     onMessage(callback) {
+        this.hashList = this.hashList.slice(0, 999);
         this.pubSubConnection.getSubscription().then((subsciption) => {
-            subsciption.on('message', callback);
+            subsciption.on('message', (message) => this.filterDuplicate(message, callback));
         });
+    }
+    /**
+     * @param {import('@google-cloud/pubsub').Message} message
+     * @param {function} callback
+     */
+    filterDuplicate(message, callback) {
+        const hash = crypto.createHash('md5').update(message.data).digest('base64');
+        if (this.hashList.includes(hash)) {
+            message.ack();
+        } else {
+            this.hashList.unshift(hash);
+            callback(message);
+        }
     }
 }
 
