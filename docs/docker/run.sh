0c57198460ce39ce28dd7b093f66d3b46a56e0e7
Jim Lind <spoon.vw@gmail.com>
2021-08-14 13:40:39 -0400

    Create proper parent directory so script can be run from anywhere

--- docs/docker/run.sh
+++ docs/docker/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+parent_directory="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 docker rm --force filmlinkd &> /dev/null
-docker build -t filmlinkd ./resources
-docker run --hostname filmlinkd.local --name filmlinkd -v `pwd`/../../:/app -it filmlinkd /bin/zsh
+docker build -t filmlinkd $parent_directory/resources
+docker run --hostname filmlinkd.local --name filmlinkd -v $parent_directory/../../:/app -it filmlinkd /bin/zsh
