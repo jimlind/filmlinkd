721c534fd731d6c6d6b88f216f5a0ca344e1356e
Jim Lind <spoon.vw@gmail.com>
2021-10-23 15:41:24 -0400

    VIP Bot Runner Tweaks
    Just for my own clarity.

--- bot-vip.js
+++ bot-vip.js
@@ -18,39 +18,37 @@ const configModel = new ConfigFactory(
     fs.existsSync,
 ).build();
 
-// Make sure the promises for PubSub and Discord Clients connect
 const container = new DependencyInjectionContainer(configModel);
+const botRestingTime = 100;
 
-// This is the part that posts RSS updates at a regular interval
 // Keeps track of if an active diary entry thread is running
 let threadRunning = false;
+// Keeps track of the process that has a recurring interval
 let interval = null;
+// Keeps track of the pagination for diary processor
 let index = 0;
 
 Promise.all([container.resolve('pubSubConnection').getSubscription()]).then(
     ([pubSubSubscription]) => {
         const startTime = Date.now();
         interval = setInterval(() => {
-            if (threadRunning) return; // Exit early
-
             if (Date.now() > startTime + 12 * 60 * 60000) {
                 container.resolve('logger').info('12 Hour Reset');
                 return process.exit();
             }
 
+            // This variable is reset after a page of entries processing has completed
+            if (threadRunning) return;
+            threadRunning = true;
+
             container
                 .resolve('diaryEntryProcessor')
-                .processPageOfVipEntries(index, 25)
+                .processPageOfVipEntries(index, 30)
                 .then((pageCount) => {
                     threadRunning = false;
                     index = pageCount === 0 ? 0 : index + pageCount;
                 });
-
-            threadRunning = true;
-
-            // Attempt to stop memory leak
-            return;
-        }, 100); // Allow bot to rest for 100ms
+        }, botRestingTime);
 
         // Clean up when process is told to end
         death((signal, error) => {
