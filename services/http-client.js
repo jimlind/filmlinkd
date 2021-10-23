4d71417eedc4157ff2ac52c26dbf162a3ccde4ab
Jim Lind <spoon.vw@gmail.com>
2021-10-23 15:40:01 -0400

    Add a header to my HTTP client
    Want to try to be a good citizen.

--- services/http-client.js
+++ services/http-client.js
@@ -1,6 +1,8 @@
 'use strict';
 
 class HttpClient {
+    headers = { 'User-Agent': 'Filmlinkd - A Letterboxd Discord Bot' };
+
     /**
      * @param {import('axios').default} axios - Library for downloading
      */
@@ -11,15 +13,19 @@ class HttpClient {
     /**
      * @param {string} url
      * @param {number} timeout
+     *
+     * @returns {Promise<import('axios').AxiosResponse>}
      */
     get(url, timeout) {
         const abort = this.axios.CancelToken.source();
         const timeoutId = setTimeout(() => abort.cancel(), timeout);
 
-        return this.axios.get(url, { cancelToken: abort.token }).then((response) => {
-            clearTimeout(timeoutId);
-            return response;
-        });
+        return this.axios
+            .get(url, { cancelToken: abort.token, headers: this.headers })
+            .then((response) => {
+                clearTimeout(timeoutId);
+                return response;
+            });
     }
 }
 
