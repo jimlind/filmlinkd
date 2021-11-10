ce67ea2918a55aa3695a5847c9ffbcc929c03c81
Jim Lind <spoon.vw@gmail.com>
2021-11-09 22:16:47 -0500

    Change letterboxd id getter to fail on no response.

--- services/letterboxd/letterboxd-letterboxd-id-web.js
+++ services/letterboxd/letterboxd-letterboxd-id-web.js
@@ -8,17 +8,21 @@ class LetterboxdLidWeb {
         this.httpClient = httpClient;
     }
 
+    /**
+     * @param {string} userName
+     * @returns {Promise<string>}
+     */
     get(userName) {
         const url = 'https://letterboxd.com/' + userName;
 
-        return this.httpClient
-            .head(url, 10000)
-            .then((response) => {
-                return response?.headers['x-letterboxd-identifier'] || '';
-            })
-            .catch(() => {
-                return '';
-            });
+        return this.httpClient.head(url, 10000).then((response) => {
+            const letterboxdId = response?.headers['x-letterboxd-identifier'] || '';
+            if (!letterboxdId) {
+                throw `Letterboxd Id Not Found on ${url}`;
+            }
+
+            return letterboxdId;
+        });
     }
 }
 
