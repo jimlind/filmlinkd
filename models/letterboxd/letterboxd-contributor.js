697c6a80219fc8a59ba00f678fc6ce452aec7a23
Jim Lind <spoon.vw@gmail.com>
2021-12-18 20:12:56 -0500

    Commit Data Objects for Contributors
    A good stepping block.
    Also attempting to follow the Letterboxd pattern more closely is going to be helpful long term.

---
+++ models/letterboxd/letterboxd-contributor.js
@@ -0,0 +1,19 @@
+/**
+ * Letterboxd API Contributor Object
+ * https://api-docs.letterboxd.com/#/definitions/Contributor
+ */
+
+class LetterboxdContributor {
+    /** @property {string} id The LID of the contributor. */
+    id = '';
+    /** @property {string} name The name of the contributor. */
+    name = '';
+    /** @property {string} tmdbid The ID of the contributor on TMDB, if known */
+    tmdbid = '';
+    /** @property {{contributions: import('./letterboxd-contribution-statistics')[]}} statistics An array of the types of contributions made, with a count of films for each contribution type. */
+    statistics = null;
+    /** @property {import('./letterboxd-link')[]} links A list of relevant URLs for this entity, on Letterboxd and external sites. */
+    links = [];
+}
+
+module.exports = LetterboxdContributor;
