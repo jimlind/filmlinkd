dca5148b7b89cbe470415f9a69a2b30f6d1cad42
Jim Lind <spoon.vw@gmail.com>
2022-01-02 00:12:51 -0500

    MOAR MODELS
    Getting some user statistics... so you know what that means. More API models.

---
+++ models/letterboxd/letterboxd-member-identifier.js
@@ -0,0 +1,11 @@
+/**
+ * Letterboxd API Member Identifier Object
+ * https://api-docs.letterboxd.com/#/definitions/MemberIdentifier
+ */
+
+class LetterboxdMemberIdentifier {
+    /** @property {string} id The LID of the member. */
+    id = '';
+}
+
+module.exports = LetterboxdMemberIdentifier;
