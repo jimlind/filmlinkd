64f0f7af09beec57ddd41503d92be0f3028758b4
Jim Lind <spoon.vw@gmail.com>
2022-01-16 22:06:35 -0500

    Fix bug with silly long user bios.

--- factories/message-embed-factory.js
+++ factories/message-embed-factory.js
@@ -236,6 +236,11 @@ class MessageEmbedFactory {
         const counts = memberStatistics.counts;
         description += `Logged films: ${counts.watches} total | ${counts.filmsInDiaryThisYear} this year`;
 
+        // There is a 4096 character limit on bios. Very rarely would we hit it, but adding some protection.
+        // Soft limit of 1000 characters of markdown text but hard limit of 4069 characters total.
+        const truncateData = { limit: 1000, ellipsis: true };
+        description = this.truncateMarkdown(description, truncateData).substring(0, 4096);
+
         const pronounList = [
             member.pronoun.subjectPronoun,
             member.pronoun.objectPronoun,
