93a479a1d6e76282160701d4bc5af6937c9c2eff
Jim Lind <spoon.vw@gmail.com>
2022-10-25 21:54:42 -0400

    Bug Fix fo LID comparison
    Also new tests.

--- services/letterboxd/letterboxd-lid-comparison.js
+++ services/letterboxd/letterboxd-lid-comparison.js
@@ -7,17 +7,25 @@ class LetterboxdLidComparison {
      * 0: A is the same as B
      * -1 A is after B
      *
+     * Strings are normally compared lowest to highest [0-9][A-Z][a-z]
+     * Letterboxd LIDs are compared lowest to highest [0-9][a-z][A-Z]
+     * So we need to swap cases before doing a comparison
+     *
      * @param {string} letterboxdIdA
      * @param {string} letterboxdIdB
      * @returns {number}
      */
     compare(letterboxdIdA, letterboxdIdB) {
+        const low = (x) => x.toLowerCase();
+        const up = (x) => x.toUpperCase();
+        const swapCase = (x) => [...x].map((c) => (c === low(c) ? up(c) : low(c))).join('');
+
         if (letterboxdIdA === letterboxdIdB) {
             return 0;
         }
 
         if (letterboxdIdA.length === letterboxdIdB.length) {
-            return letterboxdIdA < letterboxdIdB ? 1 : -1;
+            return swapCase(letterboxdIdA) < swapCase(letterboxdIdB) ? 1 : -1;
         } else {
             return letterboxdIdA.length < letterboxdIdB.length ? 1 : -1;
         }
--- test/services/letterboxd/letterboxd-lid-comparison.test.js
+++ test/services/letterboxd/letterboxd-lid-comparison.test.js
@@ -16,9 +16,19 @@ describe('Letterboxd ID Comparison', () => {
             // Different length strings
             { args: ['abcd', 'abc'], expected: -1 },
             { args: ['abc', 'abcd'], expected: 1 },
-            // Upper vs lower case strings
-            { args: ['Abc', 'abc'], expected: 1 },
-            { args: ['abc', 'Abc'], expected: -1 },
+            // Upper vs lower case vs numeric strings
+            { args: ['abc', 'Abc'], expected: 1 }, // a is before A
+            { args: ['Abc', 'abc'], expected: -1 }, // A is after a
+            { args: ['1bc', 'abc'], expected: 1 }, // 1 is before a
+            { args: ['abc', '1bc'], expected: -1 }, // a is after 1
+            { args: ['1bc', 'Abc'], expected: 1 }, // 1 is before A
+            { args: ['Abc', '1bc'], expected: -1 }, // A is after 1
+            // Actual data
+            { args: ['3hsy3B', '3m74fl'], expected: 1 }, // 3h is before 3m
+            { args: ['3m74fl', '3hsy3B'], expected: -1 }, // 3m is after 3h
+            { args: ['2bA6J9', '2Dzf87'], expected: 1 }, // 2b is before 2D
+            { args: ['2bA6J9', '2456wT'], expected: -1 }, // 2b is after 24
+            { args: ['2456wT', '2Dzf87'], expected: 1 }, // 24 is before 3D
         ];
 
         tests.forEach(({ args, expected }) => {
