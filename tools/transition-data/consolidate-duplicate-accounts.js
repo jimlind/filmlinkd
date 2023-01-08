67835d428d0bbb2b606fcfd20ddf406a8314fd24
Jim Lind <spoon.vw@gmail.com>
2023-01-08 13:26:01 -0500

    Revert "Actually Good Logic for Consolidating Duplicate Accounts"
    This reverts commit 52f29963aa0388c38d1dd78796add872aaa34a45.

--- tools/transition-data/consolidate-duplicate-accounts.js
+++ tools/transition-data/consolidate-duplicate-accounts.js
@@ -14,124 +14,46 @@ const collection = container.resolve('firestoreConnection').getCollection();
 processData(collection);
 
 async function processData(data) {
-    const fileName = 'Duplicate User Checks ' + new Date().toString() + '.txt';
-    const querySnapshot = await data.get();
+    const list = [];
 
-    // Build userName to id list
-    const actualUserRefList = {};
-    const fullDocumentList = {};
+    const querySnapshot = await data.get();
     for (const key in querySnapshot.docs) {
         const documentSnapshot = querySnapshot.docs[key];
         const data = documentSnapshot.data();
-        const userName = data.userName.toLowerCase();
-        if (!actualUserRefList[userName]) {
-            actualUserRefList[userName] = [];
-        }
-        actualUserRefList[userName].push(documentSnapshot.id);
-        fullDocumentList[documentSnapshot.id] = data;
+        const originalUserName = data.userName;
+        list[key] = data.userName;
     }
 
-    // Create documents with the lowercase document id
-    fs.writeFileSync(fileName, '11111111111111111111' + '\n', { flag: 'a+' });
-    for (const userName in actualUserRefList) {
-        const documentIdList = actualUserRefList[userName];
-        const index = documentIdList.indexOf(userName);
-        // No document with lowercase id available
-        if (index < 0) {
-            const documentId = documentIdList[0];
-            const documentData = fullDocumentList[documentId];
-            fs.writeFileSync(fileName, JSON.stringify(documentData) + '\n', { flag: 'a+' });
-
-            documentData.channelList = [];
-            const documentReference = collection.doc(userName);
-            await documentReference.set(documentData);
-            console.log(`✅ Created ${userName} from ${documentId}`);
-        }
-    }
-
-    const actualDuplicateList = {};
-    // Remove userNames that aren't duplicates
-    for (const userName in actualUserRefList) {
-        const documentIdList = actualUserRefList[userName];
-        if (documentIdList.length > 1) {
-            actualDuplicateList[userName] = documentIdList;
-        }
-    }
-
-    // Copy most recent previous data to all duplicates
-    fs.writeFileSync(fileName, '22222222222222222222' + '\n', { flag: 'a+' });
-    for (const userName in actualDuplicateList) {
-        const documentIdList = actualUserRefList[userName];
-        const actualPreviousList = [];
-        for (const key in documentIdList) {
-            const documentId = documentIdList[key];
-            actualPreviousList.push(fullDocumentList[documentId].previous);
-        }
-        const mostRecentPrevious = actualPreviousList.reduce((prev, current) => {
-            if (current?.published || 0 > prev?.published || 0) {
-                return current;
+    toFindDuplicates(list).forEach(async (username) => {
+        const fileName = 'Duplicate User Deletes ' + new Date().toString() + '.txt';
+        const indexes = list.reduce((prev, curr, index) => {
+            if (curr === username) {
+                prev.push(index);
+                return prev;
             } else {
                 return prev;
             }
+        }, []);
+
+        let mergedChannels = [];
+        indexes.forEach(async (index) => {
+            const documentSnapshot = querySnapshot.docs[index];
+            const data = documentSnapshot.data();
+            fs.writeFileSync(fileName, JSON.stringify(data) + '\n', { flag: 'a+' });
+
+            mergedChannels = mergedChannels.concat(data.channelList);
+
+            data.channelList = [];
+            // This happens async and it screws things up
+            // Don't use this willy nilly
+            //await documentSnapshot.ref.update(data);
         });
 
-        for (const key in documentIdList) {
-            const documentId = documentIdList[key];
-            const keyPublished = fullDocumentList[documentId].previous?.published;
-            if (keyPublished !== mostRecentPrevious?.published && mostRecentPrevious?.published) {
-                const documentReference = await collection.doc(documentId).get();
-                const documentData = documentReference.data();
-                fs.writeFileSync(fileName, JSON.stringify(documentData) + '\n', { flag: 'a+' });
-                documentData.previous = mostRecentPrevious;
-                await documentReference.ref.update(documentData);
-                console.log(`✅ Updated previous data for ${documentId}`);
-            }
-        }
-    }
-
-    // Consolidate channel list to the lower case document id
-    fs.writeFileSync(fileName, '33333333333333333333' + '\n', { flag: 'a+' });
-    for (const userName in actualDuplicateList) {
-        const documentIdList = actualUserRefList[userName];
-        const actualChannelList = [];
-        for (const key in documentIdList) {
-            const documentId = documentIdList[key];
-            const channelList = fullDocumentList[documentId].channelList;
-            actualChannelList.push(...channelList);
-        }
-        const cleanChannelList = [
-            ...new Map(actualChannelList.map((item) => [item['channelId'], item])).values(),
-        ];
-
-        for (const key in documentIdList) {
-            const documentId = documentIdList[key];
-            const channelList = fullDocumentList[documentId].channelList;
-            // Skipping data clearing if already empty
-            if (channelList.length == 0) {
-                continue;
-            }
-
-            // Skipping data clearing if it'll get caught in the next step
-            if (documentId === userName) {
-                continue;
-            }
-
-            // Log existing data and update with an empty channelList
-            const documentReference = await collection.doc(documentId).get();
-            const documentData = documentReference.data();
-            fs.writeFileSync(fileName, JSON.stringify(documentData) + '\n', { flag: 'a+' });
-            documentData.channelList = [];
-            await documentReference.ref.update(documentData);
-            console.log(`🚮 Deleted channel list for document ${documentId}`);
-        }
-
-        if (cleanChannelList.length !== fullDocumentList[userName].channelList.length) {
-            const documentReference = await collection.doc(userName).get();
-            const documentData = documentReference.data();
-            fs.writeFileSync(fileName, JSON.stringify(documentData) + '\n', { flag: 'a+' });
-            documentData.channelList = cleanChannelList;
-            await documentReference.ref.update(documentData);
-            console.log(`✅ Updated channel list for ${userName}`);
-        }
-    }
+        const firstDoc = querySnapshot.docs[indexes[0]];
+        const firstData = firstDoc.data();
+        firstData.channelList = mergedChannels;
+        //await firstDoc.ref.update(firstData);
+    });
 }
+
+const toFindDuplicates = (list) => list.filter((item, index) => list.indexOf(item) !== index);
