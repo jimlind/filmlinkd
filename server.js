7fc8d3c08dd3e5fa041fd3d10fe2f871057ae3c8
Jim Lind <spoon.vw@gmail.com>
2023-02-17 22:13:45 -0500

    Dumb HTTP Server
    LOL

--- server.js
+++ server.js
@@ -169,3 +169,11 @@ Promise.all([
         container.resolve('logger').info('Program Terminated', { signal, error });
     });
 });
+
+// Create a dumb http server
+const http = require('http');
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello World!');
+    response.end();
+}).listen(process.env.PORT || 8080);
