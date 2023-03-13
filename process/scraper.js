00d30b35553402ebd0a7f7d11458a47d680b618e
Jim Lind <spoon.vw@gmail.com>
2023-03-12 22:34:53 -0400

    Make the scraper actually work.

--- process/scraper.js
+++ process/scraper.js
@@ -1,4 +1,13 @@
 class Scraper {
+    /** @type {number} */
+    botRestingTime = 30000; // 30 seconds
+    /** @type {boolean} */
+    threadRunning = false;
+    /** @type {number} */
+    interval = 0;
+    /** @type {number} */
+    pageSize = 60;
+
     /**
      * @param {import('../dependency-injection-container')} container
      */
@@ -10,20 +19,39 @@ class Scraper {
     }
 
     run() {
+        // Get a random index from the user list
+        this.container
+            .resolve('subscribedUserList')
+            .getRandomIndex()
+            .then((index) => {
+                this.interval = setInterval(() => {
+                    if (this.threadRunning) return;
+                    this.threadRunning = true;
+
+                    this.container
+                        .resolve('diaryEntryProcessor')
+                        .processPageOfEntries(index, 60)
+                        .then((pageCount) => {
+                            this.threadRunning = false;
+                            index = pageCount === 0 ? 0 : index + pageCount;
+                        });
+                }, this.botRestingTime);
+            });
+
         // Listen for LogEntryResult PubSub messages posted and respond
         this.container.resolve('pubSubMessageListener').onLogEntryResultMessage((message) => {
-            // Ack the message first to remove it from the queue
-            message.ack();
-
+            // Parse the message data
             const returnData = JSON.parse(message.data.toString());
+
             // Add new users to the cached user list or update existing data
+            // The upsertResult is the most recent id for the user.
             const upsertResult = this.container
                 .resolve('subscribedUserList')
                 .upsert(returnData.userName, returnData.userLid, returnData.previousId);
 
-            // The upsertResult is the most recent id for the user.
             // Exit early if the current message isn't the most recent.
             if (upsertResult != returnData.previousId) {
+                message.ack();
                 return;
             }
 
@@ -31,13 +59,18 @@ class Scraper {
             this.container
                 .resolve('firestoreUserDao')
                 .getByUserName(returnData.userName)
-                .then((userModel) =>
-                    this.container.resolve('firestorePreviousDao').update(userModel, diaryEntry),
-                );
+                .then((userModel) => {
+                    message.ack();
+                    this.container
+                        .resolve('firestorePreviousDao')
+                        .update(userModel, returnData.diaryEntry);
+                });
         });
     }
 
     cleanUp() {
+        // Stop the recurring task
+        clearInterval(this.interval);
         // Close the PubSub connection
         this.container
             .resolve('pubSubConnection')
