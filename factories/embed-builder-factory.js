0e82b6f8815b3e94072158bb43fab47151458d80
Jim Lind <spoon.vw@gmail.com>
2023-04-11 21:03:57 -0400

    Factory Rename

--- factories/embed-builder-factory.js
+++ factories/embed-builder-factory.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
-class MessageEmbedFactory {
+class EmbedBuilderFactory {
     /**
      * @param {import('markdown-truncate')} truncateMarkdown
      * @param {import('turndown')} turndownService
@@ -12,49 +12,51 @@ class MessageEmbedFactory {
         this.turndownService = turndownService;
     }
 
-    createFollowSuccessMessage(data) {
-        return this.createEmbed()
+    createFollowSuccessEmbed(data) {
+        return this.createEmbedBuilder()
             .setDescription(
                 `I am now following ${data.displayName} (${data.userName}).\nI'll try to post their most recent entry in the appropriate channel.`,
             )
             .setThumbnail(data.image);
     }
 
-    createDuplicateFollowMessage(data) {
-        return this.createEmbed()
+    createDuplicateFollowEmbed(data) {
+        return this.createEmbedBuilder()
             .setDescription(
                 `I was previously following ${data.displayName} (${data.userName}).\nWe are already BFFs.`,
             )
             .setThumbnail(data.image);
     }
 
-    createNoAccountFoundMessage(userName) {
-        return this.createEmbed().setDescription(`I can't find **${userName}** on Letterboxd.`);
+    createNoAccountFoundEmbed(userName) {
+        return this.createEmbedBuilder().setDescription(
+            `I can't find **${userName}** on Letterboxd.`,
+        );
     }
 
-    createUnfollowedSuccessMessage(data) {
-        return this.createEmbed()
+    createUnfollowedSuccessEmbed(data) {
+        return this.createEmbedBuilder()
             .setDescription(
                 `I unfollowed ${data.displayName} (${data.userName}).\nNo hard feelings I hope.`,
             )
             .setThumbnail(data.image);
     }
 
-    createUnfollowedErrorMessage(userName) {
-        return this.createEmbed().setDescription(`Unable to unfollow ${userName}.`);
+    createUnfollowedErrorEmbed(userName) {
+        return this.createEmbedBuilder().setDescription(`Unable to unfollow ${userName}.`);
     }
 
-    createRefreshSuccessMessage(data) {
-        return this.createEmbed()
+    createRefreshSuccessEmbed(data) {
+        return this.createEmbedBuilder()
             .setDescription(`I updated my display data for ${data.displayName} (${data.userName}).`)
             .setThumbnail(data.image);
     }
 
-    createRefreshErrorMessage(userName) {
-        return this.createEmbed().setDescription(`Unable to refresh ${userName}.`);
+    createRefreshErrorEmbed(userName) {
+        return this.createEmbedBuilder().setDescription(`Unable to refresh ${userName}.`);
     }
 
-    createFollowingMessage(resultList) {
+    createFollowingEmbed(resultList) {
         const resultListSorted = resultList.sort((a, b) => {
             let lidA = a.lid;
             let lidB = b.lid;
@@ -71,20 +73,20 @@ class MessageEmbedFactory {
             return previous + string;
         }, '');
 
-        return this.createEmbed().setDescription(
+        return this.createEmbedBuilder().setDescription(
             "Here are the accounts I'm following:" + '\n' + resultTextList,
         );
     }
 
-    createEmptyFollowingMessage() {
-        return this.createEmbed().setDescription('Not following any accounts.');
+    createEmptyFollowingEmbed() {
+        return this.createEmbedBuilder().setDescription('Not following any accounts.');
     }
 
     /**
      * @param {import("../models/diary-entry")} entry
      * @param {import("../models/user")} data
      */
-    createDiaryEntryMessage(entry, data) {
+    createDiaryEntryEmbed(entry, data) {
         const profileName = data.displayName;
         const profileURL = `https://letterboxd.com/${data.userName}/`;
         const profileImage = data.image;
@@ -121,7 +123,7 @@ class MessageEmbedFactory {
         reviewText = entry.containsSpoilers ? '||' + reviewText + '||' : reviewText;
 
         const rule = reviewTitle && reviewText ? '┈'.repeat(12) + '\n' : '';
-        const embed = this.createEmbed()
+        const embed = this.createEmbedBuilder()
             .setAuthor(authorTitle, profileImage, profileURL) // DEPRECATED!
             .setTitle(adult + entry.filmTitle + ' ' + releaseYear)
             .setURL(entry.link)
@@ -139,9 +141,9 @@ class MessageEmbedFactory {
     /**
      * @param {import('../models/letterboxd/letterboxd-member')} member
      * @param {import('../models/letterboxd/letterboxd-entry')[]} entryList
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createDiaryListMessage(member, entryList) {
+    createDiaryListEmbed(member, entryList) {
         const entryTextList = entryList.map((entry) => {
             let entryFirstLine = `[**${entry.filmName} (${entry.filmYear})**](https://boxd.it/${entry.id})`;
             let entrySecondLine = entry.date ? `${this.formatDate(entry.date)} ` : '';
@@ -157,7 +159,7 @@ class MessageEmbedFactory {
             current.height > previous.height ? current : previous,
         );
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(`Recent Diary Activity from ${member.displayName}`)
             .setURL(`https://boxd.it/${member.id}`)
             .setThumbnail(largestImage.url)
@@ -167,9 +169,9 @@ class MessageEmbedFactory {
     /**
      * @param {import('../models/letterboxd/letterboxd-film')} film
      * @param {import('../models/letterboxd/letterboxd-film-statistics')} filmStatistics
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createFilmMessage(film, filmStatistics) {
+    createFilmEmbed(film, filmStatistics) {
         let description = '';
 
         // Add tagline to description
@@ -217,7 +219,7 @@ class MessageEmbedFactory {
             {},
         );
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(`${film.name} (${film.releaseYear})`)
             .setURL(`https://boxd.it/${film.id}`)
             .setThumbnail(largestImage?.url || '')
@@ -227,9 +229,9 @@ class MessageEmbedFactory {
     /**
      * @param {import('../models/letterboxd/letterboxd-member')} member
      * @param {import('../models/letterboxd/letterboxd-member-statistics')} memberStatistics
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createUserMessage(member, memberStatistics) {
+    createUserEmbed(member, memberStatistics) {
         let description = '';
 
         // Add member location to description
@@ -262,7 +264,7 @@ class MessageEmbedFactory {
             member.pronoun.possessivePronoun,
         ];
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(`${member.displayName} (${pronounList.join('/')})`)
             .setURL(`https://boxd.it/${member.id}`)
             .setThumbnail(this.parseImage(member?.avatar?.sizes))
@@ -271,9 +273,9 @@ class MessageEmbedFactory {
 
     /**
      * @param {import('../models/letterboxd/letterboxd-list-summary')} listSummary
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createListMessage(listSummary) {
+    createListEmbed(listSummary) {
         let description = `**List of ${listSummary.filmCount} films curated by [${listSummary.owner.displayName}](https://boxd.it/${listSummary.owner.id})**\n`;
         description += this.turndownService.turndown(listSummary.description) + '\n';
 
@@ -291,7 +293,7 @@ class MessageEmbedFactory {
             description += ' ...';
         }
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(listSummary.name)
             .setURL(`https://boxd.it/${listSummary.id}`)
             .setThumbnail(this.parseImage(listSummary?.previewEntries[0]?.film?.poster?.sizes))
@@ -300,9 +302,9 @@ class MessageEmbedFactory {
 
     /**
      * @param {import('../models/letterboxd/letterboxd-log-entry')[]} logEntryList
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createLoggedMessage(logEntryList) {
+    createLoggedEmbed(logEntryList) {
         if (!logEntryList.length) {
             throw 'Empty List of Log Entry Provided';
         }
@@ -326,7 +328,7 @@ class MessageEmbedFactory {
         const firstEntry = logEntryList[0];
         const title = `${firstEntry?.owner?.displayName}'s Recent Entries for ${firstEntry?.film?.name} (${firstEntry?.film?.releaseYear})`;
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(title)
             .setThumbnail(this.parseImage(logEntryList[0]?.film?.poster?.sizes))
             .setDescription(logEntryTextList.join('\n'));
@@ -334,9 +336,9 @@ class MessageEmbedFactory {
 
     /**
      * @param {import('../models/letterboxd/letterboxd-contributor')} contributor
-     * @returns {MessageEmbed}
+     * @returns {EmbedBuilder}
      */
-    createContributorMessage(contributor) {
+    createContributorEmbed(contributor) {
         const filmographyString = contributor.statistics.contributions
             .reduce((acc, contribution) => {
                 acc.push(`**${contribution.type}**: ${contribution.filmCount}`);
@@ -351,7 +353,7 @@ class MessageEmbedFactory {
             }, [])
             .join(' | ');
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle(contributor.name)
             .setURL(`https://boxd.it/${contributor.id}`)
             .setDescription(filmographyString + '\n' + linkString);
@@ -363,12 +365,12 @@ class MessageEmbedFactory {
      * @param {number} serverCount
      * @returns
      */
-    createHelpMessage(config, userCount, serverCount) {
+    createHelpEmbed(config, userCount, serverCount) {
         const name = config.get('packageName');
         const version = config.get('packageVersion');
         const description = `${name} v${version}\nTracking ${userCount} users on ${serverCount} servers`;
 
-        return this.createEmbed()
+        return this.createEmbedBuilder()
             .setTitle('(Help!) I Need Somebody')
             .setURL('https://jimlind.github.io/filmlinkd/')
             .setDescription(description)
@@ -406,32 +408,34 @@ class MessageEmbedFactory {
             );
     }
 
-    createChannelNotFoundMessage() {
-        return this.createEmbed().setDescription('Unable to find the specified channel.');
+    createChannelNotFoundEmbed() {
+        return this.createEmbedBuilder().setDescription('Unable to find the specified channel.');
     }
 
-    createFilmNotFoundMessage() {
-        return this.createEmbed().setDescription('Unable to match a film to those search terms.');
+    createFilmNotFoundEmbed() {
+        return this.createEmbedBuilder().setDescription(
+            'Unable to match a film to those search terms.',
+        );
     }
 
-    createContributorNotFoundMessage() {
-        return this.createEmbed().setDescription(
+    createContributorNotFoundEmbed() {
+        return this.createEmbedBuilder().setDescription(
             'Unable to match a contributor to those search terms.',
         );
     }
 
-    createNoListFoundMessage() {
+    createNoListFoundEmbed() {
         const message = 'Unable to match an account list to those search terms.';
-        return this.createEmbed().setDescription(message);
+        return this.createEmbedBuilder().setDescription(message);
     }
 
-    createNoLoggedEntriesFoundMessage() {
+    createNoLoggedEntriesFoundEmbed() {
         const message = 'Unable to find that film logged for that user.';
-        return this.createEmbed().setDescription(message);
+        return this.createEmbedBuilder().setDescription(message);
     }
 
-    createEmbed() {
-        return new MessageEmbed().setColor(0xa700bd);
+    createEmbedBuilder() {
+        return new EmbedBuilder().setColor(0xa700bd);
     }
 
     formatStars(rating) {
@@ -520,4 +524,4 @@ class MessageEmbedFactory {
     }
 }
 
-module.exports = MessageEmbedFactory;
+module.exports = EmbedBuilderFactory;
