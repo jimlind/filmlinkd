7d45cae560a93e09f867a4dcc9e93d9f49374599
Jim Lind <spoon.vw@gmail.com>
2023-05-09 22:29:24 -0400

    Clean Up Message Sender
    This was a long time coming.

--- services/discord/discord-message-sender.js
+++ services/discord/discord-message-sender.js
@@ -8,60 +8,65 @@ class DiscordMessageSender {
 
     send(channelId, embed) {
         return new Promise((resolve, reject) => {
-            this.discordConnection.getConnectedClient().then((client) => {
-                const channel = client.channels.cache.find((ch) => ch.id === channelId);
-
-                if (!channel) {
-                    this.logger.debug(
-                        `Unable to Send Message: Client Can't Find Channel: '${channelId}'`,
-                    );
-                    return reject();
-                }
-
-                if (channel.type !== ChannelType.GuildText) {
-                    const metadata = {
-                        channelId,
-                        messageEmbed: embed.toJSON(),
-                    };
-                    this.logger.warn('Unable to Send Message: Not Text Channel', metadata);
-                    return reject();
-                }
-
-                if (!channel.viewable) {
-                    const metadata = {
-                        channelId,
-                        messageEmbed: embed.toJSON(),
-                    };
-                    this.logger.warn('Unable to Send Message: Channel Not Visibile', metadata);
-                    return reject();
-                }
-
-                channel
-                    .send({ embeds: [embed] })
-                    .then(() => {
-                        const metadata = {
-                            channelId,
-                            guild: channel.guild.name,
-                            channel: channel.name,
-                            messageEmbed: embed.toJSON(),
-                        };
-                        this.logger.debug('Successfully Sent Message', metadata);
-                        return resolve();
-                    })
-                    .catch(() => {
-                        const metadata = {
-                            channelId,
-                            guild: channel.guild.name,
-                            channel: channel.name,
-                            messageEmbed: embed.toJSON(),
-                        };
-                        this.logger.warn(
-                            'Unable to Send Message: Bad Channel Permissions',
-                            metadata,
-                        );
+            this.discordConnection
+                .getConnectedClient()
+                .then((client) => {
+                    return client.channels.fetch(channelId);
+                })
+                .then((channel) => {
+                    if (!channel) {
+                        // Reject without logging because not being able to fetch a channel is
+                        // totally normal in a sharded environment.
                         return reject();
-                    });
-            });
+                    }
+
+                    if (channel.type !== ChannelType.GuildText) {
+                        const metadata = {
+                            channelId,
+                            messageEmbed: embed.toJSON(),
+                        };
+                        this.logger.warn('Unable to Send Message: Not Text Channel', metadata);
+                        return reject();
+                    }
+
+                    if (!channel.viewable) {
+                        const metadata = {
+                            channelId,
+                            messageEmbed: embed.toJSON(),
+                        };
+                        this.logger.warn('Unable to Send Message: Channel Not Visible', metadata);
+                        return reject();
+                    }
+
+                    channel
+                        .send({ embeds: [embed] })
+                        .then(() => {
+                            const metadata = {
+                                channelId,
+                                guild: channel.guild.name,
+                                channel: channel.name,
+                                messageEmbed: embed.toJSON(),
+                            };
+                            this.logger.debug('Successfully Sent Message', metadata);
+                            return resolve();
+                        })
+                        .catch(() => {
+                            const metadata = {
+                                channelId,
+                                guild: channel.guild.name,
+                                channel: channel.name,
+                                messageEmbed: embed.toJSON(),
+                            };
+                            this.logger.warn(
+                                'Unable to Send Message: Bad Channel Permissions',
+                                metadata,
+                            );
+                            return reject();
+                        });
+                })
+                .catch((e) => {
+                    return reject();
+                });
         });
     }
 }
