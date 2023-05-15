c0d474c444fc2cbf4bf99ac10baf375b664e8b1f
Jim Lind <spoon.vw@gmail.com>
2023-05-14 22:49:50 -0400

    Recreate Scraper to more align with VIP Scraper
    Bunch of funniness happening here that we can hopefully resolve.
    Also a method that I want to delete but it'll take some time to get there.

--- process/scraper.js
+++ process/scraper.js
@@ -11,6 +11,8 @@ class Scraper {
     resetInterval = 0;
     /** @type {number} */
     pageSize = 60;
+    /** @type {number} */
+    index = 0;
 
     /**
      * @param {import('../dependency-injection-container')} container
@@ -24,54 +26,58 @@ class Scraper {
 
     run() {
         // Get a random index from the user list
-        this.container
-            .resolve('subscribedUserList')
-            .getRandomIndex()
-            .then((index) => {
-                this.fetchInterval = setInterval(() => {
-                    if (this.fetchThreadRunning) return;
-                    this.fetchThreadRunning = true;
+        const getRandomIndex = this.container.resolve('subscribedUserList').getRandomIndex();
+        getRandomIndex.then((randomIndex) => {
+            // The recurringFetchTask uses and udates this
+            this.index = randomIndex;
 
-                    this.container
-                        .resolve('diaryEntryProcessor')
-                        .processPageOfEntries(index, 60)
-                        .then((pageCount) => {
-                            this.fetchThreadRunning = false;
-                            index = pageCount === 0 ? 0 : index + pageCount;
-                        });
-                }, this.fetchRestingTime);
-            });
+            const fetchTask = this.recurringFetchTask.bind(this);
+            this.fetchInterval = setInterval(fetchTask, this.fetchRestingTime);
 
+            const resetTAsk = this.recurringResetTask.bind(this);
+            this.resetInterval = setInterval(resetTAsk, this.resetRestingTime);
+        });
+
+        // HOPEFULLY DEPRECATED
+        // Because the version of the primary bot that is running still expects this to be listening to
+        // these events I need to keep it around for a bit longer to be backward compatible.
+        //
         // Listen for LogEntryResult PubSub messages posted and respond
         this.container.resolve('pubSubMessageListener').onLogEntryResultMessage((message) => {
+            // Acknowledge the message to remove it from the queue
+            message.ack();
+
             // Parse the message data
             const returnData = JSON.parse(message.data.toString());
-
-            // Add new users to the cached user list or update existing data
-            // The upsertResult is the most recent id for the user.
-            const upsertResult = this.container
-                .resolve('subscribedUserList')
-                .upsert(returnData.userName, returnData.userLid, returnData.previousId);
-
-            // Exit early if the current message isn't the most recent.
-            if (upsertResult != returnData.previousId) {
-                message.ack();
-                return;
-            }
 
             // Write the current (most recent) message to the database
             this.container
                 .resolve('firestoreUserDao')
                 .getByUserName(returnData.userName)
                 .then((userModel) => {
-                    message.ack();
+                    // Exit early if the existing diary entry id is older than the incoming diary entry id
+                    if ((userModel?.previous?.id || 0) >= returnData.previousId) {
+                        return;
+                    }
+
                     this.container
                         .resolve('firestorePreviousDao')
                         .update(userModel, returnData.diaryEntry);
                 });
         });
+    }
 
-        this.resetInterval = setInterval(this.recurringResetTask.bind(this), this.resetRestingTime);
+    recurringFetchTask() {
+        // This variable is reset after a page of entries processing has completed
+        if (this.fetchThreadRunning) return;
+        this.fetchThreadRunning = true;
+        this.container
+            .resolve('diaryEntryProcessor')
+            .processPageOfEntries(this.index, this.pageSize)
+            .then((pageCount) => {
+                this.fetchThreadRunning = false;
+                this.index = pageCount === 0 ? 0 : this.index + pageCount;
+            });
     }
 
     recurringResetTask() {
