2d4e5e5b74200e651846ad8ddf606f2033886761
Jim Lind <spoon.vw@gmail.com>
2023-07-04 18:16:37 -0400

    Update list-all-vip-channels.js

--- tools/test/list-all-vip-channels.js
+++ tools/test/list-all-vip-channels.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// Allow switching between dev and prod
 process.env.npm_config_live = process.argv[2] == 'prod' || false;
 const config = require('../../config.js');
 if (process.env.npm_config_live) {
