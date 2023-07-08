be9795f73761fb18af71668f970533423667c2c3
Jim Lind <spoon.vw@gmail.com>
2023-07-08 11:43:44 -0400

    Update server.mjs
    Work with awilix with es modules, and a couple other fixes.

--- server.mjs
+++ server.mjs
@@ -2,10 +2,10 @@
 
 import config from './config.js';
 import container from './dependency-injection-container.js';
-import { Scraper } from './process/scraper.mjs';
+import Scraper from './process/scraper.mjs';
 import Cluster from './process/sharding/cluster.js';
 import StandAlone from './process/standalone.js';
-import { Vip } from './process/vip.mjs';
+import Vip from './process/vip.mjs';
 
 switch (config.get('mode')) {
     case 'sharded':
@@ -14,19 +14,26 @@ switch (config.get('mode')) {
         break;
 
     case 'scraper':
-        const solorScraper = new Scraper(container(config));
-        solorScraper.run();
+        const scraperAwilixContainerPromise = container(config).initialize();
+        scraperAwilixContainerPromise.then((container) => {
+            new Scraper(container).run();
+        });
         break;
 
     case 'vip':
-        const vip = new Vip(container(config));
-        vip.run();
+        const vipAwilixContainerPromise = container(config).initialize();
+        vipAwilixContainerPromise.then((container) => {
+            new Vip(container).run();
+        });
         break;
 
     default:
-        const standalone = new StandAlone(container(config));
-        standalone.run();
-        const scraper = new Scraper(container(config));
-        scraper.run();
+        const defaultAwilixContainerPromise = container(config).initialize();
+        defaultAwilixContainerPromise.then((container) => {
+            const standalone = new StandAlone(container);
+            standalone.run();
+            const scraper = new Scraper(container);
+            scraper.run();
+        });
         break;
 }
