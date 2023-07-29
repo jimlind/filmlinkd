0328046d18ad8128b21f37b772ff24075df57f9f
Jim Lind <spoon.vw@gmail.com>
2023-07-28 23:18:04 -0400

    Create delete-one-patron.mjs
    An easier way to remove the footer for a user.

---
+++ tools/patreon/delete-one-patron.mjs
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import { FieldValue } from '@google-cloud/firestore';
+
+import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline/promises';
+import { URL } from 'url';
+import config from '../../config.mjs';
+import container from '../../dependency-injection-container.mjs';
+
+// Setup input
+const rl = readline.createInterface({ input, output });
+const userName = await rl.question('Username of account to remove footer? ');
+rl.close();
+
+// Configure as production
+const dir = new URL('.', import.meta.url).pathname;
+config.loadFile(dir + '../../config/production.json');
+config.set('googleCloudIdentityKeyFile', dir + '../../.gcp-key.json');
+
+// Initialize container
+const initializedContainer = await container(config).initialize();
+
+// ...and go!
+const collection = await initializedContainer.resolve('firestoreConnection').getCollection();
+processData(collection, userName);
+
+async function processData(collection, userName) {
+    const query = collection.where('userName', '==', userName).limit(1);
+    const querySnapshot = await query.get();
+    const documentSnapshot = querySnapshot?.docs?.[0];
+
+    if (!documentSnapshot) {
+        return;
+    }
+
+    // Remove footer field
+    documentSnapshot.ref.update({ footer: FieldValue.delete() });
+}
