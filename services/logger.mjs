9d77bea3c529a0ef4e9a6b0e372d77dbb56ed371
Jim Lind <spoon.vw@gmail.com>
2024-01-24 01:08:20 -0500

    Drastically reduce logging.
    Returning the logger should help garbage collection if it might think it needs to hold on to args. I've long suspected the logging was problematic so this should help let me see the difference.

--- services/logger.mjs
+++ services/logger.mjs
@@ -2,7 +2,7 @@ export default class Logger {
     constructor(config, winston, googleCloudWinstonTransport, consoleTransport) {
         // Create a Winston logger that logs debug and above
         this.winstonLogger = winston.createLogger({
-            level: 'debug',
+            level: 'warn',
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.prettyPrint(),
@@ -17,15 +17,15 @@ export default class Logger {
     }
 
     debug(...args) {
-        this.winstonLogger.debug(...args);
+        return this.winstonLogger.debug(...args);
     }
     info(...args) {
-        this.winstonLogger.info(...args);
+        return this.winstonLogger.info(...args);
     }
     warn(...args) {
-        this.winstonLogger.warn(...args);
+        return this.winstonLogger.warn(...args);
     }
     error(...args) {
-        this.winstonLogger.error(...args);
+        return this.winstonLogger.error(...args);
     }
 }
