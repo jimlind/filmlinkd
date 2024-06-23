2884d4a734e56393ad2497cd7d2e2c896133af61
Jim Lind <spoon.vw@gmail.com>
2024-06-22 20:07:18 -0400

    Increase Clusters Again
    There are 6500 servers to allowing 8 clusters per shard means everything is on the same shard so hopefully that helps.
    This is assuming that there is a problem somehow with the messages getting send between clusters.

--- process/sharding/cluster.mjs
+++ process/sharding/cluster.mjs
@@ -24,7 +24,7 @@ export default class Cluster {
         // totalShards:'auto' and shardsPerClusters:4 and 3,999 servers means 1 cluster with 4 shards
         // totalShards:'auto' and shardsPerClusters:2 and 3,999 servers means 2 clusters each with 2 shards
         this.manager.totalShards = 'auto';
-        this.manager.shardsPerClusters = 4;
+        this.manager.shardsPerClusters = 8;
         this.manager.mode = 'process';
 
         // Setup the secret manager manually because we are avoiding the overhead of the
