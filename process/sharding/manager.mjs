440c3d5e07d2839239190dcc342633bb5a835907
Jim Lind <spoon.vw@gmail.com>
2024-07-04 17:10:23 -0400

    debug: add sharding debugging

--- process/sharding/manager.mjs
+++ process/sharding/manager.mjs
@@ -1,5 +1,6 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import { ShardingManager } from 'discord.js';
+import container from '../../dependency-injection-container.mjs';
 import SecretManager from '../../services/google/secret-manager.mjs';
 
 export default class Manager {
@@ -23,6 +24,28 @@ export default class Manager {
             token,
             respawn: true,
             mode: 'process',
+        });
+
+        const initializedContainer = await container(this.config).initialize();
+        const logger = initializedContainer.resolve('logger');
+
+        manager.on('shardCreate', (shard) => {
+            shard.on('reconnecting', () => {
+                logger.info(`Reconnecting shard: [${shard.id}]`);
+            });
+            shard.on('spawn', () => {
+                logger.info(`Spawned shard: [${shard.id}]`);
+            });
+            shard.on('ready', () => {
+                logger.info(` Shard [${shard.id}] is ready`);
+            });
+            shard.on('death', () => {
+                logger.info(`Died shard: [${shard.id}]`);
+            });
+            shard.on('error', (err) => {
+                logger.info(`Error in  [${shard.id}] with : ${err} `);
+                shard.respawn();
+            });
         });
         manager.spawn();
     }
