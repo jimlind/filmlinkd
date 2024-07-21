76ca27f7a034b0c2e489dbe21caca87d2c44b5a5
Jim Lind <spoon.vw@gmail.com>
2024-07-21 19:30:08 -0400

    feat: types on interaction listener

--- src/services/discord/discord-interaction-listener.ts
+++ src/services/discord/discord-interaction-listener.ts
@@ -1,36 +1,32 @@
+import * as discord from 'discord.js';
+import Logger from '../logger.js';
+import DiscordConnection from './discord-connection.js';
+
 export default class DiscordInteractionListener {
-    /**
-     * @param {import('../discord/discord-connection')} discordConnection
-     * @param {import('discord.js')} discordLibrary
-     * @param {import('../../services/logger.mjs')} logger
-     */
     constructor(
-        readonly discordConnection: any,
-        readonly discordLibrary: any,
-        readonly logger: any,
+        readonly discordConnection: DiscordConnection,
+        readonly discordLibrary: typeof discord,
+        readonly logger: Logger,
     ) {}
 
     onInteraction(callback: any) {
         // Get connected client and listen for interactions
-        return this.discordConnection.getConnectedClient().then((client: any) => {
-            client.on(
-                'interactionCreate',
-                (/** @type {import('discord.js').Interaction} */ interaction: any) => {
-                    // Ignore if not a command and not from a guild member
-                    if (!interaction.isCommand()) return;
-                    if (!(interaction.member instanceof this.discordLibrary.GuildMember)) return;
+        return this.discordConnection.getConnectedClient().then((client: discord.Client<true>) => {
+            client.on('interactionCreate', (interaction: discord.Interaction) => {
+                // Ignore if not a command and not from a guild member
+                if (!interaction.isCommand()) return;
+                if (!(interaction.member instanceof this.discordLibrary.GuildMember)) return;
 
-                    return interaction
-                        .deferReply()
-                        .then(() => {
-                            callback(interaction);
-                        })
-                        .catch((error: any) => {
-                            const data = { error, interaction };
-                            this.logger.warn('Unable to defer reply on interaction.', data);
-                        });
-                },
-            );
+                return interaction
+                    .deferReply()
+                    .then(() => {
+                        callback(interaction);
+                    })
+                    .catch((error: unknown) => {
+                        const data = { error, interaction };
+                        this.logger.warn('Unable to defer reply on interaction.', data);
+                    });
+            });
         });
     }
 }
