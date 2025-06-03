c92f8c9a0a82b83a9adc491fe648cc40793b57c0
Jim Lind <spoon.vw@gmail.com>
2025-06-02 21:32:26 -0400

    Clear out all the code.
    Leave only the documents behind to maintain appropriate web presence.

--- src/models/letterboxd/letterboxd-list-identifier.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Letterboxd API List Identifier Object
- * https://api-docs.letterboxd.com/#/definitions/ListIdentifier
- */
-
-export default class LetterboxdListIdentifier {
-    /** @property {string} id The LID of the list. */
-    id = '';
-}
--- src/models/letterboxd/letterboxd-member-identifier.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Letterboxd API Member Identifier Object
- * https://api-docs.letterboxd.com/#/definitions/MemberIdentifier
- */
-
-export default class LetterboxdMemberIdentifier {
-    /** @property {string} id The LID of the member. */
-    id = '';
-}
