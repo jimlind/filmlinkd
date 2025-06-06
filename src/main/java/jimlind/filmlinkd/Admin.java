2574e53059d9070e5359838326b78ccfa92484c7
Jim Lind <spoon.vw@gmail.com>
2025-06-06 16:13:04 -0400

    feat: use new scraper and logger

--- src/main/java/jimlind/filmlinkd/Admin.java
+++ src/main/java/jimlind/filmlinkd/Admin.java
@@ -1,11 +1,13 @@
 package jimlind.filmlinkd;
 
-import jimlind.filmlinkd.config.SecretConfig;
+import jimlind.filmlinkd.config.SecretGetter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Admin {
   public static void main(String[] args) {
-    System.out.println("Java Admin");
-    String apiKey = SecretConfig.INSTANCE.getSecret("DISCORD_DEV_CLIENT_ID", "latest");
+    log.info("Java Admin");
+    String apiKey = SecretGetter.INSTANCE.getSecret("DISCORD_DEV_CLIENT_ID", "latest");
     System.out.println("Client Id: " + apiKey);
   }
 }
