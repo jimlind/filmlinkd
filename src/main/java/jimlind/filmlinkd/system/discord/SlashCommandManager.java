90b7705803cad6b2ccf4097dfeae24e2db18c02b
Jim Lind <spoon.vw@gmail.com>
2025-06-07 11:17:24 -0400

    Update SlashCommandManager.java
    actually do the work that the previous commit mentioned

--- src/main/java/jimlind/filmlinkd/system/discord/SlashCommandManager.java
+++ src/main/java/jimlind/filmlinkd/system/discord/SlashCommandManager.java
@@ -1,106 +1,45 @@
 package jimlind.filmlinkd.system.discord;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
+import javax.annotation.Nullable;
+import jimlind.filmlinkd.system.discord.eventHandler.Handler;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class SlashCommandManager {
+
+  private final Injector injector;
+
   @Inject
-  SlashCommandManager() {}
+  SlashCommandManager(Injector injector) {
+    this.injector = injector;
+  }
 
   public boolean process(SlashCommandInteractionEvent event) {
-    event.reply("Nothing available to handle event request.").queue();
+    Handler handler = getHandlerFromEventName(event.getName());
+    if (handler != null) {
+      handler.handleEvent(event);
+      return true;
+    }
+    event.reply("No command handler available.").queue();
     return false;
   }
-}
 
-/*
+  private @Nullable Handler getHandlerFromEventName(String name) {
+    String fragment = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    String className =
+        String.format("jimlind.filmlinkd.system.discord.eventHandler.%sHandler", fragment);
 
-import java.util.HashMap;
-import java.util.Map;
-import jimlind.filmlinkd.system.discord.eventHandler.*;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SlashCommandManager {
-  private final ContributorHandler contributorHandler;
-  private final DiaryHandler diaryHandler;
-  private final FilmHandler filmHandler;
-  private final FollowHandler followHandler;
-  private final FollowingHandler followingHandler;
-  private final HelpHandler helpHandler;
-  private final ListHandler listHandler;
-  private final LoggedHandler loggedHandler;
-  private final RefreshHandler refreshHandler;
-  private final RouletteHandler rouletteHandler;
-  private final UnfollowHandler unfollowHandler;
-  private final UserHandler userHandler;
-
-  HashMap<String, Handler> handlerMap = new HashMap<>();
-
-  @Autowired
-  public SlashCommandManager(
-      ContributorHandler contributorHandler,
-      DiaryHandler diaryHandler,
-      FilmHandler filmHandler,
-      FollowHandler followHandler,
-      FollowingHandler followingHandler,
-      HelpHandler helpHandler,
-      ListHandler listHandler,
-      LoggedHandler loggedHandler,
-      RefreshHandler refreshHandler,
-      RouletteHandler rouletteHandler,
-      UnfollowHandler unfollowHandler,
-      UserHandler userHandler) {
-    this.contributorHandler = contributorHandler;
-    this.diaryHandler = diaryHandler;
-    this.filmHandler = filmHandler;
-    this.followHandler = followHandler;
-    this.followingHandler = followingHandler;
-    this.helpHandler = helpHandler;
-    this.listHandler = listHandler;
-    this.loggedHandler = loggedHandler;
-    this.refreshHandler = refreshHandler;
-    this.rouletteHandler = rouletteHandler;
-    this.unfollowHandler = unfollowHandler;
-    this.userHandler = userHandler;
-
-    this.handlerMap = this.buildHandlerMap();
-  }
-
-  public boolean process(SlashCommandInteractionEvent event) {
-    Handler handler = this.handlerMap.get(event.getName());
-    if (handler == null) {
-      event.reply("Nothing available to handle event request.").queue();
-      return false;
+    try {
+      Class<?> clazz = Class.forName(className);
+      Object handler = injector.getInstance(clazz);
+      if (handler instanceof Handler) {
+        return (Handler) handler;
+      } else {
+        return null;
+      }
+    } catch (ClassNotFoundException e) {
+      return null;
     }
-    handler.handleEvent(event);
-    return true;
-  }
-
-  private HashMap<String, Handler> buildHandlerMap() {
-    HashMap<String, Handler> handlerHashMap = new HashMap<>();
-
-    this.putInMap(handlerHashMap, this.contributorHandler);
-    this.putInMap(handlerHashMap, this.diaryHandler);
-    this.putInMap(handlerHashMap, this.filmHandler);
-    this.putInMap(handlerHashMap, this.followHandler);
-    this.putInMap(handlerHashMap, this.followingHandler);
-    this.putInMap(handlerHashMap, this.helpHandler);
-    this.putInMap(handlerHashMap, this.listHandler);
-    this.putInMap(handlerHashMap, this.loggedHandler);
-    this.putInMap(handlerHashMap, this.refreshHandler);
-    this.putInMap(handlerHashMap, this.rouletteHandler);
-    this.putInMap(handlerHashMap, this.unfollowHandler);
-    this.putInMap(handlerHashMap, this.userHandler);
-
-    return handlerHashMap;
-  }
-
-  private void putInMap(Map<String, Handler> map, Handler handler) {
-    map.put(handler.getEventName(), handler);
   }
 }
-
- */
