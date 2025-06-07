5379b14ea073dd9b3822d345e7545538c3816503
Jim Lind <spoon.vw@gmail.com>
2025-06-06 21:42:01 -0400

    feat: use env vars and proj id

--- src/main/java/jimlind/filmlinkd/config/AppConfig.java
+++ src/main/java/jimlind/filmlinkd/config/AppConfig.java
@@ -7,17 +7,19 @@ import java.io.InputStream;
 import java.util.Properties;
 import jimlind.filmlinkd.system.google.SecretManager;
 import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
 
 @Singleton
 public class AppConfig {
   private static final Properties properties = new Properties();
   @Getter private final String discordBotToken;
+  @Getter private final String googleProjectId;
 
   @Inject
   AppConfig(SecretManager secretManager) {
-    String environment = "DEVELOPMENT";
-    String resourcesDir = environment.equals("PRODUCTION") ? "prod" : "dev";
-    String resource = String.format("%s/environment.properties", resourcesDir);
+    @Nullable String environment = System.getenv("FILMLINKD_ENVIRONMENT");
+    String env = environment == null ? "dev" : environment.equals("PRODUCTION") ? "prod" : "dev";
+    String resource = String.format("%s/environment.properties", env);
 
     try (InputStream input = AppConfig.class.getClassLoader().getResourceAsStream(resource)) {
       if (input == null) {
@@ -28,8 +30,11 @@ public class AppConfig {
       throw new RuntimeException("Error while loading properties file", e);
     }
 
+    googleProjectId = properties.getProperty(AppConstants.PROP_KEY_GOOGLE_PROJECT_ID);
+
     discordBotToken =
         secretManager.getSecret(
+            googleProjectId,
             properties.getProperty(AppConstants.PROP_KEY_DISCORD_BOT_TOKEN_SECRET_NAME));
   }
 }
