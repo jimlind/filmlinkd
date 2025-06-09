7317b07e756bdf80e5ea6a893e7d650bf4bf3d9f
Jim Lind <spoon.vw@gmail.com>
2025-06-08 21:23:41 -0400

    feat: helper to collect member

---
+++ src/main/java/jimlind/filmlinkd/system/discord/AccountHelper.java
@@ -0,0 +1,29 @@
+package jimlind.filmlinkd.system.discord;
+
+import com.google.inject.Inject;
+import jimlind.filmlinkd.system.letterboxd.api.MemberAPI;
+import jimlind.filmlinkd.system.letterboxd.model.LBMember;
+import jimlind.filmlinkd.system.letterboxd.web.MemberWeb;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.jetbrains.annotations.Nullable;
+
+public class AccountHelper {
+  private final MemberAPI memberAPI;
+  private final MemberWeb memberWeb;
+
+  @Inject
+  AccountHelper(MemberAPI memberAPI, MemberWeb memberWeb) {
+    this.memberAPI = memberAPI;
+    this.memberWeb = memberWeb;
+  }
+
+  @Nullable
+  public LBMember getMember(SlashCommandInteractionEvent event) {
+    OptionMapping accountMap = event.getInteraction().getOption("account");
+    String userName = accountMap != null ? accountMap.getAsString() : "";
+    String userLID = this.memberWeb.getMemberLIDFromUsername(userName);
+
+    return this.memberAPI.fetch(userLID);
+  }
+}
