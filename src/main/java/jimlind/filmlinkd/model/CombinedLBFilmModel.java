f4cc2e63a837b560dfa22bb6153a03f432b23c8a
Jim Lind <spoon.vw@gmail.com>
2025-06-14 09:26:40 -0400

    feat: model to combine lb models

---
+++ src/main/java/jimlind/filmlinkd/model/CombinedLBFilmModel.java
@@ -0,0 +1,11 @@
+package jimlind.filmlinkd.model;
+
+import jimlind.filmlinkd.system.letterboxd.model.LBFilm;
+import jimlind.filmlinkd.system.letterboxd.model.LBFilmStatistics;
+import jimlind.filmlinkd.system.letterboxd.model.LBFilmSummary;
+
+public class CombinedLBFilmModel {
+  public LBFilm film;
+  public LBFilmStatistics filmStatistics;
+  public LBFilmSummary filmSummary;
+}
