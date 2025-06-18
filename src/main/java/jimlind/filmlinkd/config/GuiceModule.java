1e1285cad29e565bb6de3b31b186f059c5decd3c
Jim Lind <spoon.vw@gmail.com>
2025-06-18 17:55:53 -0400

    feat: dependency updates
    make the factories singletons
    make the builders always new

--- src/main/java/jimlind/filmlinkd/config/GuiceModule.java
+++ src/main/java/jimlind/filmlinkd/config/GuiceModule.java
@@ -2,11 +2,14 @@ package jimlind.filmlinkd.config;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import jimlind.filmlinkd.factory.EmbedBuilderFactory;
+import jimlind.filmlinkd.factory.UserFactory;
 import jimlind.filmlinkd.system.DiscordSystem;
 import jimlind.filmlinkd.system.ShutdownThread;
 import jimlind.filmlinkd.system.discord.ConnectionManager;
 import jimlind.filmlinkd.system.discord.EventListener;
 import jimlind.filmlinkd.system.discord.SlashCommandManager;
+import jimlind.filmlinkd.system.discord.embedBuilder.*;
 import jimlind.filmlinkd.system.discord.eventHandler.*;
 import jimlind.filmlinkd.system.google.FirestoreManager;
 import jimlind.filmlinkd.system.google.SecretManager;
@@ -21,11 +24,24 @@ public class GuiceModule extends AbstractModule {
     bind(AppConfig.class).in(Scopes.SINGLETON);
     bind(ShutdownThread.class);
 
+    // Factories
+    bind(EmbedBuilderFactory.class).in(Scopes.SINGLETON);
+    bind(UserFactory.class).in(Scopes.SINGLETON);
+
     // Discord System Modules
     bind(DiscordSystem.class).in(Scopes.SINGLETON);
     bind(ConnectionManager.class).in(Scopes.SINGLETON);
     bind(EventListener.class).in(Scopes.SINGLETON);
     bind(SlashCommandManager.class).in(Scopes.SINGLETON);
+
+    // Discord Embed Builders
+    bind(ContributorEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(DiaryListEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(FilmEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(FollowingEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(ListEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(LoggedEmbedBuilder.class).in(Scopes.NO_SCOPE);
+    bind(UserEmbedBuilder.class).in(Scopes.NO_SCOPE);
 
     // Discord Event Handlers
     bind(ContributorHandler.class).in(Scopes.SINGLETON);
