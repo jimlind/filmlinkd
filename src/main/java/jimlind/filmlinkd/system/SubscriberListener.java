a4234fb2e5f7707a6d510bc5536b5ae9a707cdda
Jim Lind <spoon.vw@gmail.com>
2025-06-19 18:04:29 -0400

    feat: SubscriberListener with logging

---
+++ src/main/java/jimlind/filmlinkd/system/SubscriberListener.java
@@ -0,0 +1,36 @@
+package jimlind.filmlinkd.system;
+
+import com.google.cloud.pubsub.v1.Subscriber;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubscriberListener extends Subscriber.Listener {
+
+  @Override
+  public void failed(Subscriber.State from, Throwable failure) {
+    log.info("The Pub/Sub subscriber has encountered a fatal error and is shutting down.");
+    log.info(failure.toString());
+    System.exit(-1);
+  }
+
+  @Override
+  public void running() {
+    log.info("The Pub/Sub subscriber has successfully started and is running.");
+  }
+
+  @Override
+  public void starting() {
+    log.info("The Pub/Sub subscriber is starting.");
+  }
+
+  @Override
+  public void stopping(Subscriber.State from) {
+    log.info("The Pub/Sub subscriber is stopping.");
+  }
+
+  @Override
+  public void terminated(Subscriber.State from) {
+    log.info("The Pub/Sub subscriber has been terminated.");
+    System.exit(-1);
+  }
+}
