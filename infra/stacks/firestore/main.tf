c3a473489effa8dbccfac7b11e1685b2c2a6f8d5
Jim Lind <spoon.vw@gmail.com>
2025-06-28 14:17:19 -0400

    note on the database name
    ugh. i hate this.

--- infra/stacks/firestore/main.tf
+++ infra/stacks/firestore/main.tf
@@ -6,7 +6,9 @@ provider "google" {
 
 resource "google_firestore_database" "database" {
   project     = "filmlinkd"
-  name        = "(default)"
+  # The "name" should actually be "(default)" but the system doesn't want you to be doing this all the time so there is
+  # a 30-day limit on creating after it was deleted. If it is "(default)" other things work better.
+  name        = "filmlinkd-db"
   location_id = "us-central1"
   type        = "FIRESTORE_NATIVE"
   lifecycle {
