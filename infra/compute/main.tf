9b3943dc147b96ea460d1ae42664cfe56800aa4f
Jim Lind <spoon.vw@gmail.com>
2025-07-02 09:27:02 -0400

    fix: use cheaper network tier

--- infra/compute/main.tf
+++ infra/compute/main.tf
@@ -1,3 +1,8 @@
+resource "google_compute_project_default_network_tier" "project-tier" {
+  project      = "filmlinkd"
+  network_tier = "STANDARD"
+}
+
 module "network" {
   source = "../modules/network"
 }
