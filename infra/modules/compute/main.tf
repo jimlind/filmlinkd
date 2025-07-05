8009e7a1242e71eceb0bad9195c90a7a857aca29
Jim Lind <spoon.vw@gmail.com>
2025-07-05 19:54:36 -0400

    feat: build scraper vm

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -45,3 +45,45 @@ resource "google_compute_instance" "instances" {
     subnetwork = var.subnet_self_link
   }
 }
+
+resource "google_compute_instance" "instances" {
+  name         = "scraper"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  labels = {
+    container-vm = "filmlinkd-scraper"
+    name         = "filmlinkd-scraper"
+  }
+
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+      type  = "pd-standard"
+      size  = 20
+    }
+  }
+
+  # Something is required to load containers.
+  # This is not an official API and subject to change at any time.
+  # We shouldn't trust it, so the cloudbuild job repeats the process.
+  metadata = {
+    gce-container-declaration = <<-EOF
+      spec:
+        containers:
+          - name: filmlinkd
+            image: "ghcr.io/jimlind/filmlinkd/scraper-image:latest"
+        restartPolicy: Always
+    EOF
+  }
+
+  service_account {
+    email = "compute-runner@filmlinkd.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  network_interface {
+    subnetwork = var.subnet_self_link
+  }
+}
