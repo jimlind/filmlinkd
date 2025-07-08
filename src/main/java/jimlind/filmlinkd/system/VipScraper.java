25ff2d48bbdaa41c45dcc10632954daa807a29d8
Jim Lind <spoon.vw@gmail.com>
2025-07-07 22:08:55 -0400

    feat: add something else to VIP scraper
    I dunno. The general and VIP are going to share a lot of logic.

--- src/main/java/jimlind/filmlinkd/system/VipScraper.java
+++ src/main/java/jimlind/filmlinkd/system/VipScraper.java
@@ -1,5 +1,20 @@
 package jimlind.filmlinkd.system;
 
-public class VipScraper {
+import com.google.inject.Inject;
+import jimlind.filmlinkd.config.AppConfig;
+import jimlind.filmlinkd.runnable.GeneralScraperTask;
+import jimlind.filmlinkd.runnable.GeneralUserCacheClearTask;
+
+public class VipScraper extends GeneralScraper {
+
+  @Inject
+  public VipScraper(
+      AppConfig appConfig,
+      GeneralScraperTask generalScraperTask,
+      GeneralUserCache generalUserCache,
+      GeneralUserCacheClearTask generalUserCacheClearTask) {
+    super(appConfig, generalScraperTask, generalUserCache, generalUserCacheClearTask);
+  }
+
   public void start() {}
 }
