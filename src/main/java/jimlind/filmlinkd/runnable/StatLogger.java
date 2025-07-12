ed40fc0bd99a133defd4ce09f8e2dbcfc754f665
Jim Lind <spoon.vw@gmail.com>
2025-07-11 20:42:25 -0400

    fix: proper logging

--- src/main/java/jimlind/filmlinkd/runnable/StatLogger.java
+++ src/main/java/jimlind/filmlinkd/runnable/StatLogger.java
@@ -2,8 +2,6 @@ package jimlind.filmlinkd.runnable;
 
 import com.google.inject.Inject;
 import java.lang.management.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,15 +12,11 @@ public class StatLogger implements Runnable {
   private final MemoryMXBean memoryBean;
   private final ThreadMXBean threadBean;
 
-  private final NumberFormat formatter;
-
   @Inject
   public StatLogger() {
     classLoadingBean = ManagementFactory.getClassLoadingMXBean();
     memoryBean = ManagementFactory.getMemoryMXBean();
     threadBean = ManagementFactory.getThreadMXBean();
-
-    formatter = new DecimalFormat("#0.00");
   }
 
   @Override
@@ -32,30 +26,34 @@ public class StatLogger implements Runnable {
     }
 
     MemoryUsage heapUsage = memoryBean.getHeapMemoryUsage();
-    log.info(
-        "{memory-heap-used-mb:{}, memory-heap-committed-mb:{}, memory-heap-max-mb={}}",
-        formatter.format(heapUsage.getUsed() / MEGABYTE),
-        formatter.format(heapUsage.getCommitted() / MEGABYTE),
-        formatter.format(heapUsage.getMax() / MEGABYTE));
+    log.atInfo()
+        .setMessage("JVM Heap Statistics (MB)")
+        .addKeyValue("used", heapUsage.getUsed() / MEGABYTE)
+        .addKeyValue("committed", heapUsage.getCommitted() / MEGABYTE)
+        .addKeyValue("max", heapUsage.getMax() / MEGABYTE)
+        .log();
 
     MemoryUsage nonHeapUsage = memoryBean.getNonHeapMemoryUsage();
-    log.info(
-        "{memory-non-heap-used-mb:{}, memory-non-heap-committed-mb:{}, memory-non-heap-max-mb={}}",
-        formatter.format(nonHeapUsage.getUsed() / MEGABYTE),
-        formatter.format(nonHeapUsage.getCommitted() / MEGABYTE),
-        formatter.format(nonHeapUsage.getMax() / MEGABYTE));
+    log.atInfo()
+        .setMessage("JVM Non-Heap Statistics (MB)")
+        .addKeyValue("used", nonHeapUsage.getUsed() / MEGABYTE)
+        .addKeyValue("committed", nonHeapUsage.getCommitted() / MEGABYTE)
+        .addKeyValue("max", nonHeapUsage.getMax() / MEGABYTE)
+        .log();
 
-    log.info(
-        "{thread-live-qty:{}, thread-peak-qty:{}, thread-daemon-qty:{}, thread-started-qty:{}}",
-        threadBean.getThreadCount(),
-        threadBean.getPeakThreadCount(),
-        threadBean.getDaemonThreadCount(),
-        threadBean.getTotalStartedThreadCount());
+    log.atInfo()
+        .setMessage("JVM Thread Count")
+        .addKeyValue("live", threadBean.getThreadCount())
+        .addKeyValue("peak", threadBean.getPeakThreadCount())
+        .addKeyValue("daemon", threadBean.getDaemonThreadCount())
+        .addKeyValue("total-started", threadBean.getTotalStartedThreadCount())
+        .log();
 
-    log.info(
-        "{current-loaded-class-qty:{}, total-loaded-class-qty:{}, total-unloaded-class-qty:{}}",
-        classLoadingBean.getLoadedClassCount(),
-        classLoadingBean.getTotalLoadedClassCount(),
-        classLoadingBean.getUnloadedClassCount());
+    log.atInfo()
+        .setMessage("JVM Class Count")
+        .addKeyValue("live", classLoadingBean.getLoadedClassCount())
+        .addKeyValue("total-loaded", classLoadingBean.getTotalLoadedClassCount())
+        .addKeyValue("unloaded", classLoadingBean.getUnloadedClassCount())
+        .log();
   }
 }
