fa39e42b3b2e277a85af0d72fe556b9323df20f4
Jim Lind <spoon.vw@gmail.com>
2025-07-14 08:29:48 -0400

    refactor: happier codestyles
    javadoc and no import star

--- src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
+++ src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
@@ -1,7 +1,11 @@
 package jimlind.filmlinkd.system.google;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.*;
+import com.google.cloud.firestore.AggregateQuerySnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.inject.Inject;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -18,6 +22,7 @@ import jimlind.filmlinkd.system.letterboxd.utils.LidComparer;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
 
+/** Handles all Firestore communication. Should probably be split into multiple classes. */
 @Slf4j
 public class FirestoreManager {
 
@@ -26,6 +31,13 @@ public class FirestoreManager {
   private final ImageUtils imageUtils;
   private final UserFactory userFactory;
 
+  /**
+   * The constructor for this class.
+   *
+   * @param appConfig Contains application and environment variables
+   * @param imageUtils Assists in finding optimal Letterboxd images
+   * @param userFactory Creates our universal user model from Letterboxd API data or Firestorm data
+   */
   @Inject
   FirestoreManager(AppConfig appConfig, ImageUtils imageUtils, UserFactory userFactory) {
     this.appConfig = appConfig;
@@ -41,6 +53,11 @@ public class FirestoreManager {
     this.db = firestoreOptions.getService();
   }
 
+  /**
+   * Creates a new user document in Firestore from Letterboxd API data.
+   *
+   * @param member Letterboxd Member object from API
+   */
   public void createUserDocument(LbMember member) {
     String collectionId = appConfig.getFirestoreCollectionId();
     User user = userFactory.createFromMember(member);
@@ -52,6 +69,13 @@ public class FirestoreManager {
     }
   }
 
+  /**
+   * Attempts to find a user in the database.
+   *
+   * @param userLid The Letterboxd id for the user
+   * @return Returns null if no data available or returns the document snapshot from Firestorm if
+   *     available
+   */
   public @Nullable QueryDocumentSnapshot getUserDocument(String userLid) {
     String collectionId = appConfig.getFirestoreCollectionId();
     ApiFuture<QuerySnapshot> query =
@@ -64,6 +88,11 @@ public class FirestoreManager {
     }
   }
 
+  /**
+   * Gives access to the total number of user records in the database.
+   *
+   * @return The total number of user records in the database.
+   */
   public long getUserCount() {
     String collectionId = appConfig.getFirestoreCollectionId();
     ApiFuture<AggregateQuerySnapshot> query = this.db.collection(collectionId).count().get();
@@ -74,6 +103,12 @@ public class FirestoreManager {
     }
   }
 
+  /**
+   * Gives access to all documents for all active users. This should not be called regularly because
+   * it is expensive.
+   *
+   * @return A List of documents for all active users.
+   */
   public List<QueryDocumentSnapshot> getActiveUsers() {
     String collectionId = appConfig.getFirestoreCollectionId();
     ApiFuture<QuerySnapshot> query =
@@ -88,6 +123,13 @@ public class FirestoreManager {
     }
   }
 
+  /**
+   * Get all users where the specific channel is available in the list of channels in the user data
+   * record. This should associate with the users that are followed in a channel.
+   *
+   * @param channelId The discord channel id
+   * @return A list of all user documents that have the specific channel
+   */
   public List<QueryDocumentSnapshot> getUserDocumentListByChannelId(String channelId) {
     String collectionId = appConfig.getFirestoreCollectionId();
     Map<String, String> channelMap = Map.ofEntries(Map.entry("channelId", channelId));
@@ -100,6 +142,14 @@ public class FirestoreManager {
     }
   }
 
+  /**
+   * Attempts to add a channel to the user's data and ensures that we don't create duplicates.
+   *
+   * @param userLid User's Letterboxd id
+   * @param channelId Discord Channel id
+   * @return true if the action succeeded or the user already had the channel and false if any
+   *     number of failures
+   */
   public boolean addUserSubscription(String userLid, String channelId) {
     // Create user but also save snapshot to update with
     QueryDocumentSnapshot snapshot = this.getUserDocument(userLid);
@@ -137,6 +187,13 @@ public class FirestoreManager {
     return true;
   }
 
+  /**
+   * Attempts to remove a channel to the user's data.
+   *
+   * @param userLid User's Letterboxd id
+   * @param channelId Discord Channel id
+   * @return true if the action succeeded and false if any number of failures
+   */
   public boolean removeUserSubscription(String userLid, String channelId) {
     // Create user but also save snapshot to update with
     QueryDocumentSnapshot snapshot = this.getUserDocument(userLid);
@@ -165,6 +222,12 @@ public class FirestoreManager {
     return true;
   }
 
+  /**
+   * Updates the username, display name, and image based on most recent data from Letterboxd.
+   *
+   * @param member Letterboxd Member object from API
+   * @return true if the action succeeded and false if any number of failures
+   */
   public boolean updateUserDisplayData(LbMember member) {
     // Create user but also save snapshot to update with
     QueryDocumentSnapshot snapshot = this.getUserDocument(member.id);
@@ -191,6 +254,15 @@ public class FirestoreManager {
     return true;
   }
 
+  /**
+   * Updates the stored data for the most recent diary entry for a user.
+   *
+   * @param userLid User's Letterboxd id
+   * @param diaryLid Diary's Letterboxd id
+   * @param diaryPublishedDate Diary's published date
+   * @param diaryUri Diary's URI
+   * @return true if the action succeeded and false if any number of failures
+   */
   public boolean updateUserPrevious(
       String userLid, String diaryLid, long diaryPublishedDate, String diaryUri) {
 
