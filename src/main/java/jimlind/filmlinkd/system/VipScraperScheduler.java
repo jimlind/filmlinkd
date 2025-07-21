c7f111635c085a32ef88f699a3e945dfaea92604
Jim Lind <spoon.vw@gmail.com>
2025-07-21 09:14:32 -0400

    docs: consistency

--- src/main/java/jimlind/filmlinkd/system/VipScraperScheduler.java
+++ src/main/java/jimlind/filmlinkd/system/VipScraperScheduler.java
@@ -5,9 +5,20 @@ import jimlind.filmlinkd.config.AppConfig;
 import jimlind.filmlinkd.runnable.GeneralScraper;
 import jimlind.filmlinkd.runnable.GeneralUserCacheClearer;
 
-/** Sets up the scraper that runs over VIP users */
+/** Schedules the scraper that runs over VIP users. */
 public class VipScraperScheduler extends GeneralScraperScheduler {
 
+  /**
+   * Constructor for this class.
+   *
+   * <p>Currently taking in the "general" dependencies but the start method also does nothing so
+   * this is really a placeholder for now.
+   *
+   * @param appConfig Holds all the configs that determine how the system run
+   * @param generalScraper The scraper task that we are scheduling
+   * @param generalUserCache Where we store in memory versions records of latest diary entry
+   * @param generalUserCacheClearer The user clear cache
+   */
   @Inject
   public VipScraperScheduler(
       AppConfig appConfig,
@@ -17,5 +28,6 @@ public class VipScraperScheduler extends GeneralScraperScheduler {
     super(appConfig, generalScraper, generalUserCache, generalUserCacheClearer);
   }
 
+  /** Does nothing. */
   public void start() {}
 }
