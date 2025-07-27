b49c85b1eff6dad493766d6d9dadffb46d6ff962
Jim Lind <spoon.vw@gmail.com>
2025-07-27 14:07:04 -0400

    refactor: more demeter commits

--- src/main/java/jimlind/filmlinkd/system/discord/EventListener.java
+++ src/main/java/jimlind/filmlinkd/system/discord/EventListener.java
@@ -7,10 +7,12 @@ import java.util.concurrent.TimeUnit;
 import jimlind.filmlinkd.factory.ScrapedResultCheckerFactory;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.cache.CacheView;
 import org.jetbrains.annotations.NotNull;
 
 /** Custom listener for Discord events. On Ready and Slash Commands are needed here. */
@@ -28,8 +30,8 @@ public class EventListener extends ListenerAdapter {
   }
 
   @Override
-  public void onReady(ReadyEvent readyEvent) {
-    JDA jda = readyEvent.getJDA();
+  public void onReady(@NotNull ReadyEvent readyEvent) {
+    JDA jda = extractJDA(readyEvent);
 
     ShardManager manager = jda.getShardManager();
     if (manager == null) {
@@ -37,10 +39,10 @@ public class EventListener extends ListenerAdapter {
     }
 
     if (log.isInfoEnabled()) {
-      log.info("Discord Client Logged In on {} Servers", jda.getGuildCache().size());
+      log.info("Discord Client Logged In on {} Servers", extractGuildCache(jda).size());
     }
 
-    int shardId = jda.getShardInfo().getShardId();
+    int shardId = extractShardInfo(jda).getShardId();
     try (ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor()) {
       scheduler.scheduleAtFixedRate(
           scrapedResultCheckerFactory.create(shardId, manager.getShardsTotal()),
@@ -64,5 +66,17 @@ public class EventListener extends ListenerAdapter {
           .addKeyValue("event", event.getName())
           .log();
     }
+  }
+
+  private CacheView<Guild> extractGuildCache(JDA jda) {
+    return jda.getGuildCache();
+  }
+
+  private JDA extractJDA(ReadyEvent event) {
+    return event.getJDA();
+  }
+
+  private JDA.ShardInfo extractShardInfo(JDA jda) {
+    return jda.getShardInfo();
   }
 }
