c25730f166c9de3dc59b2f5aba4d8ab04a8aff2f
Jim Lind <spoon.vw@gmail.com>
2025-08-02 11:59:31 -0400

    refactor: clean up a could lingering issues

--- src/main/java/jimlind/filmlinkd/runnable/StatLogger.java
+++ src/main/java/jimlind/filmlinkd/runnable/StatLogger.java
@@ -15,10 +15,19 @@ import lombok.extern.slf4j.Slf4j;
  * most predictable for me.
  */
 @Slf4j
-@SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.LawOfDemeter"})
 public class StatLogger implements Runnable {
-  private static final long MEGABYTE = 1024L * 1024L;
+  private static final String TOTAL_LOADED_KEY = "total-loaded";
+  private static final String USED_KEY = "used";
+  private static final String COMMITTED_KEY = "committed";
+  private static final String MAX_KEY = "max";
+  private static final String MAIN_CLASS_KEY = "mainClass";
+  private static final String LIVE_KEY = "live";
+  private static final String PEAK_KEY = "peak";
+  private static final String DAEMON_KEY = "daemon";
+  private static final String TOTAL_STARTED_KEY = "total-started";
+  private static final String UNLOADED_KEY = "unloaded";
 
+  private static final long MEGABYTE = 1024L * 1024L;
   private final AppConfig appConfig;
   private final ClassLoadingMXBean classLoadingBean;
   private final MemoryMXBean memoryBean;
@@ -43,39 +52,47 @@ public class StatLogger implements Runnable {
       return;
     }
 
-    MemoryUsage heapUsage = memoryBean.getHeapMemoryUsage();
+    MemoryUsage heapUsage = getHeapMemoryUsage();
     log.atInfo()
         .setMessage("JVM Heap Statistics (MB)")
-        .addKeyValue("used", (double) heapUsage.getUsed() / MEGABYTE)
-        .addKeyValue("committed", (double) heapUsage.getCommitted() / MEGABYTE)
-        .addKeyValue("max", (double) heapUsage.getMax() / MEGABYTE)
-        .addKeyValue("mainClass", appConfig.getMainClass())
+        .addKeyValue(USED_KEY, (double) heapUsage.getUsed() / MEGABYTE)
+        .addKeyValue(COMMITTED_KEY, (double) heapUsage.getCommitted() / MEGABYTE)
+        .addKeyValue(MAX_KEY, (double) heapUsage.getMax() / MEGABYTE)
+        .addKeyValue(MAIN_CLASS_KEY, appConfig.getMainClass())
         .log();
 
-    MemoryUsage nonHeapUsage = memoryBean.getNonHeapMemoryUsage();
+    MemoryUsage nonHeapUsage = getNonHeapMemoryUsage();
     log.atInfo()
         .setMessage("JVM Non-Heap Statistics (MB)")
-        .addKeyValue("used", (double) nonHeapUsage.getUsed() / MEGABYTE)
-        .addKeyValue("committed", (double) nonHeapUsage.getCommitted() / MEGABYTE)
-        .addKeyValue("max", (double) nonHeapUsage.getMax() / MEGABYTE)
-        .addKeyValue("mainClass", appConfig.getMainClass())
+        .addKeyValue(USED_KEY, (double) nonHeapUsage.getUsed() / MEGABYTE)
+        .addKeyValue(COMMITTED_KEY, (double) nonHeapUsage.getCommitted() / MEGABYTE)
+        .addKeyValue(MAX_KEY, (double) nonHeapUsage.getMax() / MEGABYTE)
+        .addKeyValue(MAIN_CLASS_KEY, appConfig.getMainClass())
         .log();
 
     log.atInfo()
         .setMessage("JVM Thread Count")
-        .addKeyValue("live", threadBean.getThreadCount())
-        .addKeyValue("peak", threadBean.getPeakThreadCount())
-        .addKeyValue("daemon", threadBean.getDaemonThreadCount())
-        .addKeyValue("total-started", threadBean.getTotalStartedThreadCount())
-        .addKeyValue("mainClass", appConfig.getMainClass())
+        .addKeyValue(LIVE_KEY, threadBean.getThreadCount())
+        .addKeyValue(PEAK_KEY, threadBean.getPeakThreadCount())
+        .addKeyValue(DAEMON_KEY, threadBean.getDaemonThreadCount())
+        .addKeyValue(TOTAL_STARTED_KEY, threadBean.getTotalStartedThreadCount())
+        .addKeyValue(MAIN_CLASS_KEY, appConfig.getMainClass())
         .log();
 
     log.atInfo()
         .setMessage("JVM Class Count")
-        .addKeyValue("live", classLoadingBean.getLoadedClassCount())
-        .addKeyValue("total-loaded", classLoadingBean.getTotalLoadedClassCount())
-        .addKeyValue("unloaded", classLoadingBean.getUnloadedClassCount())
-        .addKeyValue("mainClass", appConfig.getMainClass())
+        .addKeyValue(LIVE_KEY, classLoadingBean.getLoadedClassCount())
+        .addKeyValue(TOTAL_LOADED_KEY, classLoadingBean.getTotalLoadedClassCount())
+        .addKeyValue(UNLOADED_KEY, classLoadingBean.getUnloadedClassCount())
+        .addKeyValue(MAIN_CLASS_KEY, appConfig.getMainClass())
         .log();
+  }
+
+  private MemoryUsage getNonHeapMemoryUsage() {
+    return memoryBean.getNonHeapMemoryUsage();
+  }
+
+  private MemoryUsage getHeapMemoryUsage() {
+    return memoryBean.getHeapMemoryUsage();
   }
 }
