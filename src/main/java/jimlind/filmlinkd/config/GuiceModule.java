f27bfbc42a82e96f1171a9217541c6039baf8534
Jim Lind <spoon.vw@gmail.com>
2025-08-19 22:02:35 -0400

    feat: properly locate all dependencies

--- src/main/java/jimlind/filmlinkd/config/GuiceModule.java
+++ src/main/java/jimlind/filmlinkd/config/GuiceModule.java
@@ -2,6 +2,9 @@ package jimlind.filmlinkd.config;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import jimlind.filmlinkd.cache.EntryCache;
+import jimlind.filmlinkd.cache.GeneralUserCache;
+import jimlind.filmlinkd.cache.VipUserCache;
 import jimlind.filmlinkd.config.modules.DiscordModule;
 import jimlind.filmlinkd.config.modules.LetterboxdModule;
 import jimlind.filmlinkd.factory.EmbedBuilderFactory;
@@ -12,17 +15,17 @@ import jimlind.filmlinkd.reciever.LogEntryMessageReceiver;
 import jimlind.filmlinkd.runnable.GeneralScraper;
 import jimlind.filmlinkd.runnable.GeneralUserCacheClearer;
 import jimlind.filmlinkd.runnable.StatLogger;
-import jimlind.filmlinkd.system.EntryCache;
-import jimlind.filmlinkd.system.GeneralScraperScheduler;
-import jimlind.filmlinkd.system.GeneralUserCache;
+import jimlind.filmlinkd.runnable.VipScraper;
+import jimlind.filmlinkd.runnable.VipUserCacheClearer;
 import jimlind.filmlinkd.system.ScrapedResultQueue;
 import jimlind.filmlinkd.system.ShutdownThread;
-import jimlind.filmlinkd.system.VipScraperScheduler;
 import jimlind.filmlinkd.system.google.PubSubSubscriberListener;
 import jimlind.filmlinkd.system.google.SecretManager;
 import jimlind.filmlinkd.system.google.firestore.FirestoreProvider;
 import jimlind.filmlinkd.system.google.firestore.UserReader;
 import jimlind.filmlinkd.system.google.firestore.UserWriter;
+import jimlind.filmlinkd.system.scraper.GeneralScraperScheduler;
+import jimlind.filmlinkd.system.scraper.VipScraperScheduler;
 
 /** Contains all the guts for dependency injection to work. */
 public class GuiceModule extends AbstractModule {
@@ -46,12 +49,17 @@ public class GuiceModule extends AbstractModule {
     // General System Modules
     bind(EntryCache.class).in(Scopes.SINGLETON);
     bind(PubSubSubscriberListener.class).in(Scopes.SINGLETON);
+    bind(StatLogger.class).in(Scopes.SINGLETON);
+
+    // Scraper Related Modules
     bind(GeneralScraperScheduler.class).in(Scopes.SINGLETON);
     bind(GeneralScraper.class).in(Scopes.SINGLETON);
     bind(GeneralUserCache.class).in(Scopes.SINGLETON);
     bind(GeneralUserCacheClearer.class).in(Scopes.SINGLETON);
     bind(VipScraperScheduler.class).in(Scopes.SINGLETON);
-    bind(StatLogger.class).in(Scopes.SINGLETON);
+    bind(VipScraper.class).in(Scopes.SINGLETON);
+    bind(VipUserCache.class).in(Scopes.SINGLETON);
+    bind(VipUserCacheClearer.class).in(Scopes.SINGLETON);
 
     // Google System Modules
     bind(SecretManager.class).in(Scopes.SINGLETON);
