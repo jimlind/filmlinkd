7f39f534df97c7959c660896069294ca4ae9d1af
Jim Lind <spoon.vw@gmail.com>
2025-10-05 19:14:07 -0400

    release: v6.0.0-beta.8.2

--- src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
@@ -89,8 +89,8 @@ public class Client {
       connection.setRequestProperty("Host", url.getHost());
       connection.setRequestProperty("User-Agent", "Filmlinkd - A Letterboxd Discord Bot");
       connection.setRequestProperty("Authorization", authorization);
-      connection.setConnectTimeout((int) TimeUnit.SECONDS.toMillis(15));
-      connection.setReadTimeout((int) TimeUnit.SECONDS.toMillis(15));
+      connection.setConnectTimeout((int) TimeUnit.SECONDS.toMillis(20));
+      connection.setReadTimeout((int) TimeUnit.SECONDS.toMillis(20));
 
       if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
         connection.disconnect();
