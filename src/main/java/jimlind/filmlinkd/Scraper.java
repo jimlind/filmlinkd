c6ab6457ad50670504ac886bbf6ef0cc37c7817e
Jim Lind <spoon.vw@gmail.com>
2025-10-25 10:35:51 -0400

    feat: kill process if in tracing mode after 120s

--- src/main/java/jimlind/filmlinkd/Scraper.java
+++ src/main/java/jimlind/filmlinkd/Scraper.java
@@ -3,6 +3,8 @@ package jimlind.filmlinkd;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import jimlind.filmlinkd.config.AppConfig;
 import jimlind.filmlinkd.config.GuiceModule;
 import jimlind.filmlinkd.google.pubsub.PubSubManagerInterface;
@@ -14,6 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 /** The main entry point for the scraper application. */
 @Slf4j
 public final class Scraper {
+
+  public static final String TRACING_MODE = "tracing";
+
   private Scraper() {}
 
   /**
@@ -43,5 +48,17 @@ public final class Scraper {
 
     // Register shutdown events
     Runtime.getRuntime().addShutdownHook(injector.getInstance(ShutdownThread.class));
+
+    // Shutdown After 60 seconds if in tracing mode
+    if (TRACING_MODE.equals(System.getProperty("app.mode"))) {
+      Executors.newSingleThreadScheduledExecutor()
+          .schedule(
+              () -> {
+                System.out.println("Killing process after 120 seconds...");
+                System.exit(0);
+              },
+              60,
+              TimeUnit.SECONDS);
+    }
   }
 }
