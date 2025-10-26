d8301b9b6be2d57f168a9ce792266ad328406b7d
Jim Lind <spoon.vw@gmail.com>
2025-10-25 21:03:45 -0400

    test: patch the secret manager to not fail
    should throw a lot of errors
    i'm fine with that

--- src/main/java/jimlind/filmlinkd/google/secret/SecretManager.java
+++ src/main/java/jimlind/filmlinkd/google/secret/SecretManager.java
@@ -5,14 +5,17 @@ import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Handles all things related to the Secret service. Currently only getting because I set the
  * secrets myself using other methods.
  */
 @Singleton
+@Slf4j
 public class SecretManager implements SecretManagerInterface {
-  private final SecretManagerServiceClient client;
+  private @Nullable SecretManagerServiceClient client;
 
   /** The constructor for this class. */
   @Inject
@@ -20,7 +23,7 @@ public class SecretManager implements SecretManagerInterface {
     try {
       this.client = SecretManagerServiceClient.create();
     } catch (IOException e) {
-      throw new IllegalStateException("Failed to create SecretManagerServiceClient", e);
+      log.error("Failed to create SecretManagerServiceClient", e);
     }
   }
 
@@ -32,6 +35,11 @@ public class SecretManager implements SecretManagerInterface {
    * @return The string value of the secret
    */
   public String getSecret(String projectId, String secretName) {
+    if (client == null) {
+      log.error("Attempting to fetch a secret while SecretManagerServiceClient is null");
+      return String.format("dummy-secret-for-%s-in-%s", secretName, projectId);
+    }
+
     String secretPath =
         String.format("projects/%s/secrets/%s/versions/latest", projectId, secretName);
     AccessSecretVersionRequest request =
