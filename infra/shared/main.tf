f530d9c6ce34235a50779718bf0538f8c83447cc
Jim Lind <spoon.vw@gmail.com>
2025-10-31 19:12:01 -0400

    test: remove network module

--- infra/shared/main.tf
+++ infra/shared/main.tf
@@ -3,9 +3,9 @@ resource "google_compute_project_default_network_tier" "project-tier" {
   network_tier = "STANDARD"
 }
 
-module "network" {
-  source = "../modules/network"
-}
+# module "network" {
+#   source = "../modules/network"
+# }
 
 module "firestore" {
   source = "../modules/firestore"
