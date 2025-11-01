914609784fe340dfb68e39bc1db7633cd0a5fcf5
Jim Lind <spoon.vw@gmail.com>
2025-11-01 13:16:27 -0400

    fix: set defaults for firestore

--- infra/modules/firestore/main.tf
+++ infra/modules/firestore/main.tf
@@ -9,6 +9,10 @@ resource "google_firestore_database" "database" {
   name        = "(default)"
   location_id = "us-central1"
   type        = "FIRESTORE_NATIVE"
+
+  point_in_time_recovery_enablement = "POINT_IN_TIME_RECOVERY_DISABLED"
+  deletion_policy                   = "ABANDON"
+
   lifecycle {
     prevent_destroy = true
   }
