145e3c4de2da9d72d3d6443c8ef8da5e7d77123b
Jim Lind <spoon.vw@gmail.com>
2025-11-02 09:34:41 -0500

    fix: put outputs back on shared
    i'm not sure if these are really really needed at this point since the dev env is grabbing them from state instead of output

--- infra/shared/outputs.tf
+++ infra/shared/outputs.tf
@@ -1,4 +1,4 @@
-# output "subnet_self_link" {
-#   description = "The self_link of the shared subnet."
-#   value       = module.network.subnet_self_link
-# }
+output "subnet_self_link" {
+  description = "The self_link of the shared subnet."
+  value       = module.network.subnet_self_link
+}
