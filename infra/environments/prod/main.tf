bae78c1f34d6350f9dd12b39228dbe5e6150a649
Jim Lind <spoon.vw@gmail.com>
2025-11-02 18:04:37 -0500

    fix: more ram for scraper

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -16,6 +16,6 @@ module "scraper-production" {
   app              = "scraper"
   label            = "filmlinkd-scraper-production"
   machine_type     = "e2-small"
-  max_heap_size    = "512"
+  max_heap_size    = "1024"
   subnet_self_link = data.terraform_remote_state.shared.outputs.subnet_self_link
 }
