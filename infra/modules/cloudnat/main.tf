8312895490e1960455fb4434ec0f83cb0ffa01b5
Jim Lind <spoon.vw@gmail.com>
2025-11-02 09:31:24 -0500

    fix: enforce defauts in NAT

--- infra/modules/cloudnat/main.tf
+++ infra/modules/cloudnat/main.tf
@@ -2,6 +2,9 @@ resource "google_compute_router" "compute_router" {
   name    = "compute-route"
   region  = "us-central1"
   network = var.network_self_link
+
+  description                   = ""
+  encrypted_interconnect_router = false
 }
 
 resource "google_compute_router_nat" "compute_router_nat" {
@@ -10,6 +13,14 @@ resource "google_compute_router_nat" "compute_router_nat" {
   region                             = "us-central1"
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  type                                 = "PUBLIC"
+  udp_idle_timeout_sec                 = 30
+  tcp_established_idle_timeout_sec     = 1200
+  tcp_time_wait_timeout_sec            = 120
+  icmp_idle_timeout_sec                = 30
+  tcp_transitory_idle_timeout_sec      = 30
+  source_subnetwork_ip_ranges_to_nat64 = null
 
   log_config {
     enable = true
