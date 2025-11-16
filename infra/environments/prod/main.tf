bf82badb224522c3cdf548016ec407e1249b9f19
Jim Lind <spoon.vw@gmail.com>
2025-11-15 20:51:55 -0500

    feat: use a smaller container

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -1,14 +1,10 @@
-# might be interesting to try some custom e2 instances if we can
-# the bot doesn't need all the CPU that's available to it
-# "e2-custom-small-4096"
-
 module "bot-production" {
   source           = "../../modules/compute"
   environment      = "PRODUCTION"
   name             = "bot-production"
   app              = "bot"
   label            = "filmlinkd-bot-production"
-  machine_type     = "e2-medium"
+  machine_type     = "e2-custom-small-4096"
   max_heap_size    = "1024"
   subnet_self_link = data.terraform_remote_state.shared.outputs.subnet_self_link
 }
