511eff4064d5cf2d0b611fdd6fe44c4d55440697
Jim Lind <spoon.vw@gmail.com>
2026-01-17 12:12:13 -0500

    Add core dependency injection files

---
+++ src/main/java/jimlind/filmlinkd/core/di/ApplicationComponent.java
@@ -0,0 +1,22 @@
+package jimlind.filmlinkd.core.di;
+
+import dagger.Component;
+import java.util.Map;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import jimlind.filmlinkd.config.AppConfig;
+import jimlind.filmlinkd.discord.event.handler.Handler;
+import jimlind.filmlinkd.system.DiscordSystem;
+
+/** Dagger component for the application. */
+@Singleton
+@Component(modules = {HandlerModule.class})
+public interface ApplicationComponent {
+  Map<String, Provider<Handler>> handlerMap();
+
+  // Contains application and environment variables
+  AppConfig appConfig();
+
+  // The primary interface for Discord systems
+  DiscordSystem discordSystem();
+}
---
+++ src/main/java/jimlind/filmlinkd/core/di/HandlerModule.java
@@ -0,0 +1,25 @@
+package jimlind.filmlinkd.core.di;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.StringKey;
+import jimlind.filmlinkd.discord.event.handler.ContributorHandler;
+import jimlind.filmlinkd.discord.event.handler.Handler;
+
+@Module
+public class HandlerModule {
+  @Provides
+  @IntoMap
+  @StringKey("contributor")
+  Handler provideContributionHandler(ContributorHandler handler) {
+    return handler;
+  }
+
+  //  @Provides
+  //  @IntoMap
+  //  @StringKey("diary")
+  //  Handler provideDiaryHandler(DiaryHandler handler) {
+  //    return handler;
+  //  }
+}
